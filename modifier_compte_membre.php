3304a73f4b667b9e24cf32a5cb438e263a10dc15
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2024-10-17 11:28:19 +0200

    Creation des nouvelles pages

--- modifier_compte_membre.php
+++ modifier_compte_membre.php
@@ -4,8 +4,80 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier compte membre</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+<header>
+        <div class="logo">
+            <img src="images/logoBlanc.png" alt="PACT Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#" class="active">Accueil</a></li>
+                <li><a href="#">Publier</a></li>
+                <li><a href="#">Mon Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+    <footer class="footer_detail_avis">
+        <div class="newsletter">
+            <div class="newsletter-content">
+                <h2>Inscrivez-vous à notre Newsletter</h2>
+                <p>PACT</p>
+                <p>Redécouvrez la Bretagne !</p>
+                <form class="newsletter-form">
+                    <input type="email" placeholder="Votre adresse mail" required>
+                    <button type="submit">S'inscrire</button>
+                </form>
+            </div>
+            <div class="newsletter-image">
+                <img src="images/Boiteauxlettres.png" alt="Boîte aux lettres">
+            </div>
+        </div>
+        
+        <div class="footer-links">
+            <div class="logo">
+                <img src="images/logoBlanc.png" alt="Logo PACT">
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">Mentions Légales</a></li>
+                    <li><a href="#">RGPD</a></li>
+                    <li><a href="#">Nous connaître</a></li>
+                    <li><a href="#">Nos partenaires</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Publier</a></li>
+                    <li><a href="#">Historique</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">CGU</a></li>
+                    <li><a href="#">Signaler un problème</a></li>
+                    <li><a href="#">Nous contacter</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">Presse</a></li>
+                    <li><a href="#">Newsletter</a></li>
+                    <li><a href="#">Notre équipe</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="social-icons">
+                <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
+                <a href="#"><img src="images/Vector2.png" alt="Instagram"></a>
+                <a href="#"><img src="images/youtube.png" alt="YouTube"></a>
+                <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
