487574dc90c341630f66666423c73f5d74de825e
quguen <quguen@pce-test>
2024-10-22 15:31:26 +0200

    Creation PHP

--- voir_offres.php
+++ voir_offres.php
@@ -2,6 +2,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+    <?php session_start(); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>En-tête PAVCT</title>
@@ -11,16 +12,17 @@
     <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
-<header class="header-pc">
+    <div class="header-membre">
+    <header class="header-pc">
         <div class="logo-pc">
             <img src="images/logoBlanc.png" alt="PACT Logo">
         </div>
         
         <nav>
             <ul>
-                <li><a href="#" class="active">Accueil</a></li>
+                <li><a href="#" >Accueil</a></li>
                 <li><a href="#">Publier</a></li>
-                <li><a href="#">Mon Compte</a></li>
+                <li><a href="#" class="active">Mon Compte</a></li>
             </ul>
         </nav>
     </header>
@@ -30,6 +32,31 @@
         </div>
         
     </header>
+    </div>
+    
+
+    <div class="header-pro">
+    <header class="header-pc">
+        <div class="logo-pc">
+            <img src="images/logoBlanc.png" alt="PACT Logo">
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href="#" >Accueil</a></li>
+                <li><a href="#">Publier</a></li>
+                <li><a href="#" class="active">Mon Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+    <header class="header-tel">
+        <div class="logo-tel">
+            <img src="images/logoNoir.png" alt="PACT Logo">
+        </div>
+        
+    </header>
+    </div>
+
     <main class="toute_les_offres_main">
         <header>
             <h2>Les offres</h2>
@@ -37,49 +64,46 @@
                 <button><img src="images/tri.png">Filtrer</button>
             </span> -->
         </header>
+
         <section id="offers-list">
-            <article class="offer">
-                <img src="images/offre1.png" alt="Image de l'offre Golf de St-Samson">
-                <div class="offer-details">
-                    <h2>Golf de St-Samson</h2>
-                    <p>Plœmeur Bodou</p>
-                    <span>1 mois</span>
-                    <span>
-                        <!-- <img src="images/etoile.png" class="img-etoile">
-                        <p>4.6 <span class="nb_avis">(355 avis)</span></p> -->
-                    </span>
-                    <button>Voir l'offre →</button>
-                </div>
-            </article>
 
-            <article class="offer">
-                <img src="images/offre2.png" alt="Image de l'offre Armor'Park">
-                <div class="offer-details">
-                    <h2>Armor'Park</h2>
-                    <p>Lannion</p>
-                    <span>3 mois</span>
-                    <span>
-                        <!-- <img src="images/etoile.png" class="img-etoile">
-                        <p>4 <span class="nb_avis">(50 avis)</span></p> -->
-                    </span>
-                    <button>Voir l'offre →</button>
-                </div>
-            </article>
 
-            <article class="offer">
-                <img src="images/offre3.png" alt="Image de l'offre L'Eclipse">
-                <div class="offer-details">
-                    <h2>L'Eclipse</h2>
-                    <p>Lannion</p>
-                    <span>6 mois</span>
-                    <span>
-                        <!-- <img src="images/etoile.png" class="img-etoile">
-                        <p>3.8 <span class="nb_avis">(300 avis)</span></p> -->
-                    </span>
-                    <button>Voir l'offre →</button>
-                </div>
-            </article>
+
+        <?php
+            $postgresdb = "postgresdb";
+            $dbname = "5432";
+            $user = "sae";
+            $pass = "DB_ROOT_PASSWORD";
+
+            try {
+                $dbh = new PDO("$driver:host=$server;dbname=$dbname", $user, $pass);
+                $stmt = $dbh->prepare('SELECT * FROM _offre WHERE professionnel = :professionnel');
+                $stmt->execute(['professionnel' => $_SESSION["compte"]]);
+                
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {?>
+                        <article class="offer">
+                            <img src="images/<?php echo htmlspecialchars($row['url_image']); ?>.png" alt="<?php echo htmlspecialchars($row['titre_offre']); ?>">
+                            <div class="offer-details">
+                                <h2><?php echo htmlspecialchars($row['titre_offre']); ?></h2>
+                                <p><?php echo htmlspecialchars($row['ville']); ?></p>
+                                <span><?php echo date_format(date_create($row['date_publication']), 'd/m/Y'); ?></span>
+                                <span>
+                                    <!-- <img src="images/etoile.png" class="img-etoile">
+                                    <p><?php /*echo round($row['note_moyenne'], 1); */?><span class="nb_avis">(355 avis)</span></p> -->
+                                </span>
+                                <button>Voir l'offre →</button>
+                            </div>
+                        </article>
+        <?php
+            }
+            } catch (PDOException $e) {
+                print "Erreur!: ". $e->getMessage(). "<br/>";
+                die();
+            }
+            ?>
+
         </section>
+
     </main>
     <nav class="nav-bar">
     <a href="#"><img src="images/icones/House.png" alt="image de maison"></a>
