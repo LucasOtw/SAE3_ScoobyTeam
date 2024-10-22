f9442b79d2443474009d276f295e402c682f9b93
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-10-22 18:45:23 +0200

    Update deconnexion_compte_pro.php Redirection

--- deconnexion_compte_pro.php
+++ deconnexion_compte_pro.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset(); // Supprime toutes les variables de session
 session_destroy(); // Détruit la session active
-header("Location: login.php"); // Redirection vers la page de connexion
+header("Location: connexion_pro.php"); // Redirection vers la page de connexion
 exit();
 ?>
