eabb3555c7cb98c2f0d3a5cb002793d100682aa5
quguen <quguen@pce-test>
2024-10-23 09:24:48 +0200

    Renommage dossiers images, php

--- creation_offre3.php
+++ creation_offre3.php
@@ -13,7 +13,7 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#">Accueil</a></li>
+                <li><a href="">Accueil</a></li>
                 <li><a href="#" class="active">Publier</a></li>
                 <li><a href="#">Mon Compte</a></li>
             </ul>
