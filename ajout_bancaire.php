3dc66ac1a5ba49d17c4c235e1b78d0cffcf79d66
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-10-23 08:24:42 +0200

    changement du nom du CSS

--- ajout_bancaire.php
+++ ajout_bancaire.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Premières coordonnées bancaires</title>
-    <link rel="stylesheet" href="style_bancaire.css">
+    <link rel="stylesheet" href="saisir_info_bancaire.css">
 </head>
 <body>
 <header>
