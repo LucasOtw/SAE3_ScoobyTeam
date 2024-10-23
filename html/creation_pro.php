85032d029dfca36174e77f559b8c8ef2a3025b81
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-10-23 14:18:10 +0200

    Update creation_pro.php

--- html/creation_pro.php
+++ html/creation_pro.php
@@ -136,7 +136,7 @@ session_start();
                 </form>
                 <div class="creation_compte_pro_other-links">
                     <p>Déjà un compte ? <a href="connexion_pro.php">Connexion</a></p>
-                    <p>S’inscrire avec un compte <a href="creation_membre.php">Membre</a></p>
+                    <p>S’inscrire avec un compte <a href="creation_compte_membre.php">Membre</a></p>
                 </div>
             </div>
             <div class="image-section">
