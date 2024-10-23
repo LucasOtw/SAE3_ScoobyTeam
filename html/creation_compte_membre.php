2b52a9c6b8cf2ebee29b9f7357d6ae83ed7d25a3
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-10-23 15:57:49 +0200

    Update creation_compte_membre.php

--- html/creation_compte_membre.php
+++ html/creation_compte_membre.php
@@ -21,9 +21,9 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">Publier</a></li>
-                <li><a href="#" class="active">Mon Compte</a></li>
+                <li><a href="voir_offres.php">Accueil</a></li>
+                <li><a href="connexion_pro.php">Publier</a></li>
+                <li><a href="connexion_membre.php" class="active">Mon Compte</a></li>
             </ul>
         </nav>
     </header>
