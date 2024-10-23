ded9cb4fef9135541de77d749b4e9bea79047bf0
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-10-23 13:01:48 +0200

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -142,8 +142,8 @@
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Publier</a></li>
+                    <li><a href="voir_offres.php">Accueil</a></li>
+                    <li><a href="connexion_pro.php">Publier</a></li>
                     <li><a href="#">Historique</a></li>
                 </ul>
             </div>
