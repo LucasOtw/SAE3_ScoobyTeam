9f147c68425cd097cafa5d668f4911240fc9b29c
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-10-23 17:03:45 +0200

    Update creation_offre_spectacle.php fixe spectacle

--- html/creation_offre_spectacle.php
+++ html/creation_offre_spectacle.php
@@ -53,7 +53,7 @@
                     <div class="col">
                         <fieldset>
                             <legend style="display:block;">Type de l'offre</legend>
-                            <input type="text" id="type_offre" name="type_offre" placeholder="Parc d'attractions" disabled>
+                            <input type="text" id="type_offre" name="type_offre" placeholder="Spectacle" disabled>
                         </fieldset>
                     </div>
                 </div>
@@ -121,25 +121,7 @@
                     </div>
                 </div>
 
-                <!-- Age requis -->
-                <div class="row">
-                    <div class="col">
-                        <fieldset>
-                            <legend>Age minimum requis</legend>
-                            <input type="number" id="age" name="age" placeholder="Age minimum requis">
-                        </fieldset>
-                    </div>  
-                </div>
-
-                <!-- Nb attractions -->
-                <div class="row">
-                    <div class="col">
-                        <fieldset>
-                            <legend>Nombre d'attractions</legend>
-                            <input type="number" id="nb_attraction" name="nb_attractions" placeholder="Nombre d'attractions">
-                        </fieldset>
-                    </div>  
-                </div>
+            
 
                 <!-- Tags -->
                 <div class="row">
