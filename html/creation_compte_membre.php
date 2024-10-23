aff0b5d89fca0466c899cbae8911918e9c043b78
Odin <odin.rajic@etudiant.univ-rennes.fr>
2024-10-23 13:20:05 +0200

    Update creation_compte_membre.php

--- html/creation_compte_membre.php
+++ html/creation_compte_membre.php
@@ -245,7 +245,7 @@
                 $insert = $dbh -> prepare("insert into membre(telephone, mail, mdp, nom, prenom, pseudo, adresse_postal, complement_adresse, code_postal, ville)
                                                 values ($telephone, $email, $passwordHashed, $nom, $prenom, $pseudo, $adresse, $complementAdresse, $codePostal, $ville)");
     
-                $_SESSION["compte"]= ($dbh->query("SELECT currval('_compte_code_compte_seq');"))->fetch();
+                $_SESSION["compte"]= ($dbh->query("SELECT currval('tripenarvor._compte_code_compte_seq');"))->fetch();
                 var_dump($_SESSION["compte"]);
             } else {
                 // Affiche les erreurs
