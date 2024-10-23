d4428dc118a7567dba25bf18aed69c757bbe6078
vasamson <samson.valentin2005@gmail.com>
2024-10-23 10:02:21 +0200

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    //<?php session_start(); ?>
+    /*<?php session_start(); ?>*/
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>En-tête PAVCT</title>
