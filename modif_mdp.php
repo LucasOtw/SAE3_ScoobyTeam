a171fb8642a22e96c6e648332bf4e6ba8405dd7a
vasamson <samson.valentin2005@gmail.com>
2024-10-23 08:29:06 +0200

    Update modif_mdp.php

--- modif_mdp.php
+++ modif_mdp.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier mdp compte membre</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="modif_mdp.css">
 </head>
 <body>
 <header>
