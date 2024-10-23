72016353b56b9c4acdfb1ca6bc49c309ec20a769
Odin <odin.rajic@etudiant.univ-rennes.fr>
2024-10-23 15:50:00 +0200

    Update connexion_membre.php

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -103,14 +103,14 @@
     
             $existeUser = $dbh->prepare("SELECT code_compte FROM tripenarvor._compte WHERE mail='$email'");
             $existeUser->execute();
-            var_dump($existeUser);
 
-            /*if($existeUser){
+            if($existeUser){
                 // si l'utilisateur existe, on vérifie d'abord si il est membre.
+                $existeUser = $existeUser->fetch();
                 // Car même si l'adresse mail et le mdp sont corrects, si le compte n'est pas lié à un membre, ça ne sert à rien de continuer les vérifications
                 $existeMembre = $dbh->prepare("SELECT 1 FROM tripenarvor._membre WHERE code_compte = :code_compte");
-                $existeMembre->bindParam(':code_compte',$existeUser->fetch()['code_compte']);
-            }*/
+                $existeMembre->bindParam(':code_compte',$existeUser);
+            }
             
             // $mailDansBdd = $dbh -> prepare("select code_compte from tripenarvor._membre where mail='$email';");
             // $mailDansBdd -> execute();
