8dcbfe84bfa8e66383be68bef493e6bddcfa3472
vasamson <samson.valentin2005@gmail.com>
2024-10-23 10:07:25 +0200

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    /*<?php session_start(); ?>*/
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Les offres</title>
