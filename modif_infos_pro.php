c7f196d821a80b727746e0a00edc5c07956567bd
anprieur <antoine.prieur@yahoo.fr>
2024-11-19 15:16:00 +0100

    Update modif_infos_pro.php

--- modif_infos_pro.php
+++ modif_infos_pro.php
@@ -64,8 +64,8 @@
         </fieldset>
     </div>
     <div class="compte_membre_save_delete">
-    <button type="submit" class="submit-btn1">Supprimer mon compte</button>
-    <a href="deconnexion_compte_pro.php" class="submit-btn2">Déconnexion</a>
+    /*<button type="submit" class="submit-btn1">Supprimer mon compte</button>*/
+    <a href="deconnexion_compte_pro.php" class="submit-btn1">Déconnexion</a>
     <button type="submit" class="submit-btn3">Enregistrer</button>
 </div>
 
