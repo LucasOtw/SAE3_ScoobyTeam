091b96b7019ae21339a283108c5c49128fd9f658
parzival <152271525+Parzival35@users.noreply.github.com>
2024-11-19 14:25:13 +0100

    Ajouts fichiers etape 1 formulaire creation d'offre

---
+++ etape_1/creation_offre_activite_1.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Offre Activité</title>
+    <link rel="stylesheet" href="../creation_offre2.css">
+
+
+</head>
+<body>
+    <header class="header_pro">
+        <div class="logo">
+            <img src="../../images/logo_blanc_pro.png" alt="PACT Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="mes_offres.php">Accueil</a></li>
+                <li><a href="#" class="active">Publier</a></li>
+                <li><a href="informations_personnelles_pro.php">Mon Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+     <div class="fleche_retour">
+        <div>
+            <a href="creation_offre1.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
+        </div>
+    </div>
+
+    <div class="header-controls">
+        <div>
+            <img id="etapes" src="../images/FilArianne2.png" alt="Étapes" width="80%" height="80%">
+        </div>
+    </div>
+
+
+    <!-- Main Section -->
+    <main class="main-creation-offre2">
+
+        <div class="form-container2">
+            <h1>Publier une offre</h1>
+
+            <!-- Form Fields -->
+            <form action="" method="post" enctype="multipart/form-data" onsubmit="checkFermeture()">
+                <!-- Establishment Name & Type -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Nom de l'activité *</legend>
+                            <input type="text" id="nom_offre" name="nom_offre" placeholder="Nom de l'activité *">
+                        </fieldset>
+                    </div>
+                    <div class="col">
+                        <fieldset>
+                            <legend style="display:block;">Type de l'offre</legend>
+                            <input type="text" id="type_offre" name="type_offre" placeholder="Activité" disabled>
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Email & Phone -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Email *</legend>
+                            <input type="email" id="email" name="email" placeholder="Email *">
+                        </fieldset>            
+                    </div>
+                    <div class="col">                     
+                        <fieldset>
+                            <legend>Téléphone *</legend>
+                            <input type="tel" id="telephone" name="telephone" placeholder="Téléphone *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Localisation -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Adresse Postale *</legend>
+                            <input type="text" id="adresse" name="adresse" placeholder="Adresse Postale *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Complément d'Adresse</legend>
+                            <input type="text" id="complement_adresse" name="complement_adresse" placeholder="Complément d'Adresse ">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Ville *</legend>
+                            <input type="text" id="ville" name="ville" placeholder="Ville *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Code Postal *</legend>
+                            <input type="text" id="code_postal" name="code_postal" placeholder="Code Postal *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Prix -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Tarif *</legend>
+                            <input type="text" id="prix" name="prix" placeholder="Tarif *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Lien du site -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Ajouter le lien du site (facultatif)</legend>
+                            <input type="url" id="lien" name="lien" placeholder="Ajouter le lien du site (facultatif)">
+                        </fieldset>
+                    </div>  
+                </div>
+
+                <!-- Photos -->
+                <div class="row">
+                    <div class="col">
+                        <label for="photos">Photos (facultatif)</label>
+                        <input type="file" id="photos" name="photos[]" multiple>
+                    </div>
+                </div>
+
+                <!-- Age requis -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Age minimum requis *</legend>
+                            <input type="number" id="age" name="age" placeholder="Age minimum requis *">
+                        </fieldset>
+                    </div>  
+                </div>
+
+                <!-- Durée de l'activité -->
+                <div class="row">
+                <div class="col">
+                        <fieldset class="duree">
+                            <legend style="display:block;">Durée de l'activité</legend>
+                            <input type="time" id="duree" name="duree" placeholder="Durée de l'activité">
+                        </fieldset>
+                    </div>
+                </div>
+                
+
+                <!-- Résumé -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Résumé (facultatif)</legend>
+                            <input type="text" id="resume" name="resume" placeholder="Résumé (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Description -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Description (facultatif)</legend>
+                            <input type="text" id="description" name="description" placeholder="Description (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+
+                <!-- Accessibilite -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Accessibilité (facultatif)</legend>
+                            <input type="text" id="accessibilite" name="accessibilite" placeholder="Accessibilité (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+
+                <!-- Prestations incluses -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Prestations incluses (facultatif)</legend>
+                            <input type="text" id="prestations_incluses" name="prestations_incluses" placeholder="Prestations incluses (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+
+                <!-- Prestations non-incluses -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Prestations incluses (facultatif)</legend>
+                            <input type="text" id="prestations_non_incluses" name="prestations_non_incluses" placeholder="Prestations non-incluses (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <button type="submit" id="button_valider">
+                    Continuer <img src="../images/fleche.png" alt="Fleche" width="25px" height="25px">
+                </button>
+
+            </form>
+        </div>
+    </main>
+</body>
+</html>
