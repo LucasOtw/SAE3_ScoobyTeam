626b7c2eb784fc049576f40d6b6566052a151b4d
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-20 13:45:39 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -209,6 +209,7 @@ function tempsEcouleDepuisPublication($offre){
             $infosOffre = $infosOffre->fetchAll();
 
             foreach($infosOffre as $offre){
+                var_dump($offre);
                 // Récupérer la ville
                 $villeOffre = $dbh->prepare('SELECT ville FROM tripenarvor._adresse WHERE code_adresse = :code_adresse');
                 $villeOffre->bindParam(":code_adresse", $offre["code_adresse"]);
