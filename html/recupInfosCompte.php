da030fd57eb8c7947fa905416635b96e9216320f
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-20 17:49:50 +0100

    Update recupInfosCompte.php

--- html/recupInfosCompte.php
+++ html/recupInfosCompte.php
@@ -1,1 +1,35 @@
+<?php
 
+  $dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
+  $username = "sae";
+  $password = "philly-Congo-bry4nt";
+  
+  // Créer une instance PDO
+  $dbh = new PDO($dsn, $username, $password);
+
+  // on récupère les infos du compte actuelle
+  if(isset($_SESSION['membre']){
+     $compte = $_SESSION['membre'];
+
+     // on sélectionne les infos du membre
+
+    $monCompte = $dbh->prepare("SELECT * FROM tripenarvor._membre WHERE code_compte = :code_compte");
+    $monCompte->bindValue(":code_compte",$_SESSION['membre']['code_compte']);
+    $monCompte->execute();
+
+    $monCompte = $monCompte->fetch(PDO::FETCH_ASSOC);
+  } elseif(isset($_SESSION['pro'])){
+     $compte = $_SESSION['pro'];
+
+    // on sélectionne les infos du pro
+     $monCompte = $dbh->prepare("SELECT * FROM tripenarvor._professionnel WHERE code_compte = :code_compte");
+     $monCompte->bindValue(":code_compte",$compte);
+     $monCompte->execute();
+  }
+
+echo "<pre>";
+var_dump($compte);
+var_dump($monCompte);
+echo "</pre">;
+
+?>
