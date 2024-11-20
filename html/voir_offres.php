038734e1aab6cec3f38bad7a285a6f5342d513d4
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-11-20 12:22:30 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -66,7 +66,7 @@ function tempsEcouleDepuisPublication($offre){
     <div id="customPopup">
         <img src=images/connexion.png  width=50px height=50px >
         <p>Créez votre compte en quelques clics et accédez à un monde de possibilités ! </p>
-        <a id="connexion" href="connexion_membre.php" id="bouton_connexion">Se connecter</a>
+        <a id="connexion" href="connexion_membre.php" id="bouton_connexion">S'inscrire</a>
         <img id="closePopup" src="images/erreur.png" width=15px height=15px>
     </div>
 
