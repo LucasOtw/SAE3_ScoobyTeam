8c41f944a16ac24982bb3241fe8959ea0fb30a03
parzival <152271525+Parzival35@users.noreply.github.com>
2024-11-20 14:41:53 +0100

    Ajout étapes formulaire création offre

---
+++ etape_1_form/creation_offre_restaurant_1.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Offre Restaurant</title>
+    <link rel="stylesheet" href="../creation_offre2.css">
+
+
+</head>
+<body>
+    <header class="header_pro">
+        <div class="logo">
+            <img src="../../images/logo_blanc_pro.png" alt="PACT Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="mes_offres.php">Accueil</a></li>
+                <li><a href="#" class="active">Publier</a></li>
+                <li><a href="informations_personnelles_pro.php">Mon Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+     <div class="fleche_retour">
+        <div>
+            <a href="creation_offre1.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
+        </div>
+    </div>
+
+    <div class="header-controls">
+        <div>
+            <img id="etapes" src="../images/fil_ariane1.png" alt="Étapes" width="80%" height="80%">
+        </div>
+    </div>
+
+
+    <!-- Main Section -->
+    <main class="main-creation-offre2">
+
+        <div class="form-container2">
+            <h1>Publier une offre</h1>
+
+            <!-- Form Fields -->
+            <form action="" method="post" enctype="multipart/form-data" onsubmit="checkFermeture()">
+                <!-- Establishment Name & Type -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Nom du Restaurant *</legend>
+                            <input type="text" id="nom_offre" name="nom_offre" placeholder="Nom du Restaurant *">
+                        </fieldset>
+                    </div>
+                    <div class="col">
+                        <fieldset>
+                            <legend style="display:block;">Type de l'offre</legend>
+                            <input type="text" id="type_offre" name="type_offre" placeholder="Restauration" disabled>
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Email & Phone -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Email *</legend>
+                            <input type="email" id="email" name="email" placeholder="Email *">
+                        </fieldset>            
+                    </div>
+                    <div class="col">                     
+                        <fieldset>
+                            <legend>Téléphone *</legend>
+                            <input type="tel" id="telephone" name="telephone" placeholder="Téléphone *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Localisation -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Adresse Postale *</legend>
+                            <input type="text" id="adresse" name="adresse" placeholder="Adresse Postale *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Complément d'Adresse</legend>
+                            <input type="text" id="complement_adresse" name="complement_adresse" placeholder="Complément d'Adresse ">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Ville *</legend>
+                            <input type="text" id="ville" name="ville" placeholder="Ville *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Code Postal *</legend>
+                            <input type="text" id="code_postal" name="code_postal" placeholder="Code Postal *">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Lien du site -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Ajouter le lien du site (facultatif)</legend>
+                            <input type="url" id="lien" name="lien" placeholder="Ajouter le lien du site (facultatif)">
+                        </fieldset>
+                    </div>  
+                </div>
+
+                <!-- Photos -->
+                <div class="row">
+                    <div class="col">
+                        <label for="photos">Photos (facultatif)</label>
+                        <input type="file" id="photos" name="photos[]" multiple>
+                    </div>
+                </div>
+
+                <!-- Résumé -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Résumé (facultatif)</legend>
+                            <input type="text" id="resume" name="resume" placeholder="Résumé (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Description -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Description (facultatif)</legend>
+                            <input type="text" id="description" name="description" placeholder="Description (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+
+                <!-- Accessibilite -->
+                <div class="row">
+                    <div class="col">
+                        <fieldset>
+                            <legend>Accessibilité (facultatif)</legend>
+                            <input type="text" id="accessibilite" name="accessibilite" placeholder="Accessibilité (facultatif)">
+                        </fieldset>
+                    </div>
+                </div>
+
+                <!-- Tags -->
+                <div class="row">
+                    <div class="col">
+
+                        <label for="tags">Tags</label>
+
+                        <div class="dropdown-container">
+                            <button type="button" class="dropdown-button">Sélectionner des tags</button>
+                            <div class="dropdown-content">
+
+                            <?php
+                                    $dbh = new PDO("host=postgresdb;port=5432;dbname=db-scooby-team", "sae", "philly-Congo-bry4nt");
+
+                                    foreach($dbh->query('SELECT nom_tag from tripenarvor._son_tag natural join tripenarvor._tags where restauration = true', PDO::FETCH_ASSOC) as $row)
+                                    {
+                                    ?>
+                                <label class="tag">
+                                    <input type="checkbox" name="tags" value="<?php echo $row; ?>">
+                                    <?php echo ucfirst($row); ?>
+                                </label>
+                                    <?php
+                                    }
+                                ?>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <button type="submit" id="button_valider">
+                    Continuer <img src="../images/fleche.png" alt="Fleche" width="25px" height="25px">
+                </button>
+
+            </form>
+        </div>
+    </main>
+</body>
+</html>
---
+++ etape_2_restau/creation_offre_restaurant_2.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Offre Restaurant</title>
+    <link rel="stylesheet" href="../creation_offre2.css">
+
+
+</head>
+<body>
+    <header class="header_pro">
+        <div class="logo">
+            <img src="../../images/logo_blanc_pro.png" alt="PACT Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="mes_offres.php">Accueil</a></li>
+                <li><a href="#" class="active">Publier</a></li>
+                <li><a href="informations_personnelles_pro.php">Mon Compte</a></li>
+            </ul>
+        </nav>
+    </header>
+     <div class="fleche_retour">
+        <div>
+            <a href="creation_offre1.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
+        </div>
+    </div>
+
+    <div class="header-controls">
+        <div>
+            <img id="etapes" src="../images/fil_ariane2.png" alt="Étapes" width="80%" height="80%">
+        </div>
+    </div>
+
+
+    <!-- Main Section -->
+    <main class="main-creation-offre2">
+
+        <div class="form-container2">
+            <h1>Publier une offre</h1>
+
+            <!-- Form Fields -->
+            <form action="" method="post" enctype="multipart/form-data" onsubmit="checkFermeture()">
+                <!-- Price Options -->
+                <div class="price-options">
+                    <label for="prix">Prix</label>
+                        <div class="radio-group">
+                            <div>
+                                <input type="radio" id="moins_25" name="prix" value="€">
+                                <label class="label-check" for="moins_25">€ (menu à moins de 25€)</label>
+                            </div>
+                            <div>
+                                <input class="label-check" type="radio" id="entre_25_40" name="prix" value="€€">
+                                <label class="label-check" for="entre_25_40">€€ (entre 25€ et 40€)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="plus_40" name="prix" value="€€€">
+                                <label class="label-check" for="plus_40">€€€ (au-delà de 40€)</label>
+                            </div>
+                        </div>
+                </div>
+
+                <!-- Meal Options -->
+                <div class="meal-options">
+                    <label>Options de repas</label>
+                    <div class="checkbox-group">
+                        <div>
+                            <input type="checkbox" id="petit_dejeuner" name="repas[]" value="petit_dejeuner">
+                            <label class="label-check" for="petit_dejeuner">Petit-Déjeuner</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="brunch" name="repas[]" value="brunch">
+                            <label class="label-check" for="brunch">Brunch</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="dejeuner" name="repas[]" value="dejeuner" checked>
+                            <label class="label-check" for="dejeuner">Déjeuner</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="diner" name="repas[]" value="diner" checked>
+                            <label class="label-check" for="diner">Dîner</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="boissons" name="repas[]" value="boissons">
+                            <label class="label-check" for="boissons">Boissons</label>
+                        </div>
+                    </div>
+                </div>
+
+                <button type="submit" id="button_valider">
+                    Continuer <img src="../images/fleche.png" alt="Fleche" width="25px" height="25px">
+                </button>
+
+            </form>
+        </div>
+    </main>
+</body>
+</html>
