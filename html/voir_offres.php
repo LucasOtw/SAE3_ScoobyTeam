c8c0e3926018f68ca0db27640037cec5f38b6ed5
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-11-20 12:00:27 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -64,9 +64,9 @@ function tempsEcouleDepuisPublication($offre){
     <!-- Conteneur du pop-up -->
     <div id="customPopup">
         <img src=images/connexion.png  width=50px height=50px >
-        <img src=images/erreur.png width=15px height=15px>
+        <img id="closePopup" src=images/erreur.png width=15px height=15px>
         <p>Créez votre compte en quelques clics et accédez à un monde de possibilités ! </p>
-        <button id="closePopup">Fermer</button>
+        <button >Fermer</button>
     </div>
 
     <script>
