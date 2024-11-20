bed9acd53933ae2fabb280d85b86e2285d8ae43f
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-20 12:09:33 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -66,7 +66,7 @@ function tempsEcouleDepuisPublication($offre){
         <img src=images/connexion.png  width=50px height=50px >
         <p>Créez votre compte en quelques clics et accédez à un monde de possibilités ! </p>
         <a href="connexion_membre.php" id="bouton_connexion>Se connecter</a>
-        <img id="closePopup" src=images/erreur.png width=15px height=15px>
+        <img id="closePopup" src="images/erreur.png" width=15px height=15px>
     </div>
 
     <script>
