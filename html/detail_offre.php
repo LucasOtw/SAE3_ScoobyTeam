48b3bcce6c59740472e24f2d57f0afdb42de53b2
anprieur <antoine.prieur@yahoo.fr>
2024-11-21 11:25:43 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -46,7 +46,6 @@
              {
                  $type_offre = "restauration";
                  $details_offre = $offre_r->fetch(PDO::FETCH_ASSOC);
-                 echo $details_offre["gamme_prix"];
              }
              else if (!empty($offre_p))
              {
