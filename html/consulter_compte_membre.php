66581b3f3bbd816ace1efa49d6e75335507e4aa9
vasamson <samson.valentin2005@gmail.com>
2024-11-21 11:20:28 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conxsulter compte membre</title>
+    <title>Consulter compte membre</title>
     <link rel="stylesheet" href="consulter_compte_membre.css">
 </head>
 <body>
