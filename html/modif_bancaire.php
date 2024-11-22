f808af29694d265f65b851e0f1480b962ee0e293
parzival <152271525+Parzival35@users.noreply.github.com>
2024-11-22 13:41:00 +0100

    Update modif_bancaire.php

--- html/modif_bancaire.php
+++ html/modif_bancaire.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier coordonnées bancaires</title>
-    <link rel="stylesheet" href="modif_bancaire.css">
+    <link rel="stylesheet" href="modif_bancaire.css?">
     
 </head>
 
