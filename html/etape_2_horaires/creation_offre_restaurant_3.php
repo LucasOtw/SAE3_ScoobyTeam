fb555e4c84a8dc929e94ac251e3649bd87dc6a9c
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-22 15:36:05 +0100

    Update creation_offre_restaurant_3.php

--- html/etape_2_horaires/creation_offre_restaurant_3.php
+++ html/etape_2_horaires/creation_offre_restaurant_3.php
@@ -297,10 +297,8 @@
         
             // si il y a une horaire d'ouverture ou de fermeture, on la met
             if (!empty($horaire_ouverture) || !empty($horaire_fermeture)) {
-                if(strtotime($horaire_ouverture) <= strtotime($horaire_fermeture)){
+                if(strtotime($horaire_ouverture) >= strtotime($horaire_fermeture)){
                     $erreurs[] = $jour." : L'heure d'ouverture doit être plus ancienne que l'heure de fermeture !";
-                } else {
-                    echo "hein ?";
                 }
                 if(empty($erreurs)){
                     $horaires_par_jour[$jour] = [
