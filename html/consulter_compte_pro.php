22bc61f855901a06052290d61f38fe2feee43bb4
jmulliez <jmulliez@pce-test>
2024-11-22 11:16:04 +0100

    update

--- html/consulter_compte_pro.php
+++ html/consulter_compte_pro.php
@@ -1,43 +1,67 @@
+<?php
+
+ob_start();
+session_start();
+
+include("recupInfosCompte.php");
+
+
+if(isset($_GET["deco"])){
+    session_unset();
+    session_destroy();
+    header('location: connexion_pro.php');
+    exit;
+}
+if(!isset($_SESSION['pro'])){
+   header('location: connexion_pro.php');
+   exit;
+}
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier mdp compte membre</title>
-    <link rel="stylesheet" href="consulter_compte_pro.css">
+    <title>Mes offres</title>
+    <link rel="stylesheet" href="mes_offres.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header>
+     <header>
         <div class="logo">
-            <img src="images/logoBlanc.png" alt="PACT Logo">
+            <img src="images/logoBlanc.png" alt="PAVCT Logo">
         </div>
         <nav>
             <ul>
-                <li><a href="#">Accueil</a></li>
-                <li><a href="#">Publier</a></li>
-                <li><a href="#" class="active">Mon Compte</a></li>
+                <li><a href="mes_offres.php" class="active">Accueil</a></li>
+                <li><a href="creation_offre.php">Publier</a></li>
+                <li><a href="mes_offres.php?deco=true">Déconnexion</a></li>
             </ul>
         </nav>
     </header>
-
-    <div class="container">
-        <div class="header">
-            <img src="images/Rectangle_3.png" alt="Bannière" class="header-img">
-        </div>
-
-        <div class="profile-section">
-            <img src="images/pppro.png" alt="Photo de profil" class="profile-img">
-            <h1>Ti Al Lannec</h1>
-            <p>tiallannec@gmail.com | 07.98.76.54.12</p>
-        </div>
-
-        <div class="tabs">
-            <div class="tab active">Informations</div>
-            <div class="tab">Mot de passe et sécurité</div>
-            <div class="tab">Offres</div>
-            <div class="tab">Paiement</div>
-        </div>
-    </div>
+    <main>
+        <section class="profile">
+            <div class="banner">
+                <img src="images/Fond.png" alt="Bannière de profil">
+            </div>
+            <div class="profile-info">
+                <img class="profile-picture" src="images/hotel.jpg" alt="Profil utilisateur">
+                <h1><?php echo $monCompte['raison_sociale']; ?></h1>
+                <p><?php echo $compte['mail'] ." | ". $compte['telephone']; ?></p>
+            </div>
+        </section>
+    
+        <section class="tabs">
+            <ul>
+                <li><a href="consulter_compte_pro.php">Informations personnelles</a></li>
+                <li><a href="mes_offres.php" class="active">Mes offres</a></li>
+                <li><a href="#">Compte bancaire</a></li>
+            </ul>
+        </section>
 
     <form action="#" method="POST">
         <div class="crea_pro_raison_sociale_num_siren">
