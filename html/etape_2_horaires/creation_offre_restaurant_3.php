ae768d8d2e677532d7d529b6df5b6bc520bb691a
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 10:14:49 +0100

    Update creation_offre_restaurant_3.php

--- html/etape_2_horaires/creation_offre_restaurant_3.php
+++ html/etape_2_horaires/creation_offre_restaurant_3.php
@@ -2,16 +2,7 @@
 
 session_start();
 
-if(isset($_SESSION['crea_offre_2'])){
-   var_dump($_SESSION['crea_offre_2']);
-} else {
-   echo "AQDSFG¨£";
-}
-
-    if(isset($_POST['EnvoiEtape3'])){
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
+      if(isset($_POST['EnvoiEtape3'])){
 
         // Liste des jours et les champs associés dans $_POST
         $jours = [
