930ca01858f95a433bc074e2515b63602231e2c9
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 10:04:37 +0100

    Update creation_offre_restaurant_3.php

--- html/etape_2_horaires/creation_offre_restaurant_3.php
+++ html/etape_2_horaires/creation_offre_restaurant_3.php
@@ -323,7 +323,6 @@ if(isset($_SESSION['crea_offre_2'])){
             foreach($erreurs as $err){
                 echo $err."<br>";
             }
-           echo "t";
         } else {
            echo "Dobby has no master. Dobby is a free elf.";
         }
