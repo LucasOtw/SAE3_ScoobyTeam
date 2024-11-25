31b632ef0304bc59a4c3d9c537c5074ba5089c33
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 15:29:31 +0100

    Update consulter_compte_pro.php

--- html/consulter_compte_pro.php
+++ html/consulter_compte_pro.php
@@ -48,7 +48,8 @@ if (isset($_POST['modif_infos'])){
                    $valeurSansEspaces = trim(preg_replace('/\s+/', '', trim($valeur)));
                    $query = $dbh->prepare("UPDATE tripenarvor._compte SET $champ = :valeur WHERE code_compte = :code_compte");
                    $query->execute(['valeur' => $valeurSansEspaces, 'code_compte' => $compte['code_compte']]);
-                   var_dump($query);
+                   var_dump($valeurSansEspaces);
+                   var_dump($compte['code_compte']);
                    break;
                
                case 'telephone':
