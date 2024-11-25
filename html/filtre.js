44a794e0b3aa4447cb753a716db2107133027635
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 12:34:21 +0100

    Update filtre.js

--- html/filtre.js
+++ html/filtre.js
@@ -1,10 +1,12 @@
-// Sélection des éléments
-const autresButton = document.getElementById('autres-button');
-console.log(autresButton.innerHTML);
-const filtersSection = document.getElementById('filters-section');
-
-// Gestion du clic sur le bouton "Autres"
-autresButton.addEventListener('click', () => {
-    // Alterne entre affichage et masquage
-    filtersSection.classList.toggle('hidden');
+document.addEventListener('DOMContentLoaded', function(){
+    // Sélection des éléments
+    const autresButton = document.getElementById('autres-button');
+    console.log(autresButton.innerHTML);
+    const filtersSection = document.getElementById('filters-section');
+    
+    // Gestion du clic sur le bouton "Autres"
+    autresButton.addEventListener('click', () => {
+        // Alterne entre affichage et masquage
+        filtersSection.classList.toggle('hidden');
+    });
 });
