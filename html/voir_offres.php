04e4d3d4469a4076557ce071f1d41fd96645c01d
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 14:08:38 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -184,8 +184,7 @@ function tempsEcouleDepuisPublication($offre){
                     <option value="">Lieu</option>
                     <option value="">Lieu ></option>
                 </select>
-                <select class="search-select">
-                    <option value="">Ouverture</option>
+                <select class="search-select" placeholder="Ouverture">
                     <option value="">Ouvert</option>
                     <option value="">Fermé</option>
                 </select>
