1dc261b1ce544f4d9db43c005d4a990a68643137
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-25 09:00:07 +0100

    Update recupInfosCompte.php

--- html/recupInfosCompte.php
+++ html/recupInfosCompte.php
@@ -55,8 +55,6 @@
     $monComptePrive->execute();
 
     $monComptePrive = $monComptePrive->fetch(PDO::FETCH_ASSOC);
-    var_dump($monComptePrive);
-    var_dump($monComptePro);
     if ($monComptePrive) {
         $monComptePro['num_siren'] = $monComptePrive['num_siren'];
     }
