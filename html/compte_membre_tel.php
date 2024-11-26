7fcf0c214b724c22e85946d6419298ee7ed196bf
parzival <152271525+Parzival35@users.noreply.github.com>
2024-11-26 16:46:42 +0100

    Add files via upload

---
+++ html/compte_membre_tel.php
@@ -0,0 +1,80 @@
+<?php
+ob_start(); // bufferisation, ça devrait marcher ?
+session_start();
+
+include("recupInfosCompte.php");
+
+// if(isset($_GET['logout'])){
+//    session_unset();
+//    session_destroy();
+//    header('location: connexion_membre.php');
+//    exit;
+// }
+
+// if(!isset($_SESSION['membre'])){
+//    header('location: connexion_membre.php');
+//    exit;
+// }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Profil</title>
+    <link rel="stylesheet" href="compte_membre_tel.css">
+</head>
+<body>
+    <header class="header-tel">
+        <img class="background" src="images/image_fond_gris.png" alt="image de fond">
+        <div class="logo-tel">
+            <img src="images/LogoCouleur.png" alt="PACT Logo">
+        </div>
+        <div class="profile-info">
+            <img src="images/icones/icone_compte.png" alt="Photo de profil" class="profile-img">
+            <h1><?php echo $monCompteMembre['prenom']." ".$monCompteMembre['nom']; ?></h1>
+            <p><?php echo $mesInfos['mail']; ?> | <?php echo trim(preg_replace('/(\d{2})/', '$1 ', $mesInfos['telephone'])); ?></p>
+        </div>
+    </header>
+
+    <main class="main_compte_membre_tel">
+        <div class="actions">
+            <a href="consulter_compte_membre.php">
+                <button class="action-button">
+                    <img src="images/crayon.png" alt="crayon">
+                    Éditer les informations
+                </button>
+            </a>
+            <a href="modif_mdp_membre.php">
+                <button class="action-button">
+                    <img src="images/bouclier.png" alt="bouclier">
+                    Modifier mon mot de passe
+                </button>
+            </a>
+            <!-- <button class="action-button">📜 Historique</button> -->
+            <a href="voir_offres.php?deco=true">
+                <button class="action-button">
+                    <img src="images/power.png" alt="power">
+                    Déconnexion
+                </button>
+            </a>
+            <!-- <button class="action-button danger">🗑️ Supprimer mon compte</button> -->
+        </div>
+    </main>
+    <nav class="nav-bar">
+        <a href="voir_offres.php"><img src="images/House icon.png" alt="image de maison"></a>
+        <a href="#"><img src="images/Recent icon.png" alt="image d'horloge"></a>
+        <a href="#"><img src="images/Croix icon.png" alt="image de PLUS"></a>
+        <a href="
+            <?php
+                if(isset($_SESSION["membre"]) || !empty($_SESSION["membre"])){
+                    echo "consulter_compte_membre.php";
+                } else {
+                    echo "connexion_membre.php";
+                }
+            ?>">
+            <img src="images/User icon.png" alt="image de Personne"></a>
+    </nav>
+</body>
+</html>
