2f9af957778be9e217b730ed33b71423096d513c
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-26 10:04:49 +0100

    Update creation_offre3.php

--- html/creation_offre3.php
+++ html/creation_offre3.php
@@ -38,7 +38,7 @@ if($monComptePro['code_compte_bancaire']){
     $infosCB = $recupInfosCB->fetch(PDO::FETCH_ASSOC);
 }
 
-if(isset($_POST['valider']){
+if(isset($_POST['valider'])){
    $code_iban = $_POST['IBAN'];
 
    validerIBAN($code_iban);
