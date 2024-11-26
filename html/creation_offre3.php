0987d4aa2a920a1ff9d5ef4940658f40866ee145
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-26 11:24:43 +0100

    Update creation_offre3.php

--- html/creation_offre3.php
+++ html/creation_offre3.php
@@ -44,7 +44,7 @@ if(isset($_POST['valider'])){
    if(validerIBAN($code_iban) === true){
        echo "YOUHOU !";
    } else {
-       foreach($validerIBAN($code_iban) as $erreur){
+       foreach(validerIBAN($code_iban) as $erreur){
            echo $erreur;
        }
    }
