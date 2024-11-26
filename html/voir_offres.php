7ff90e199bde4a3d5f31193af94d0d6dbbab9878
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2024-11-26 09:58:44 +0100

    Ajout

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -60,10 +60,8 @@ function tempsEcouleDepuisPublication($offre){
     <link rel="stylesheet" href="voir_offres.css?ad">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="filtre.js"></script>
-
-    
     <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <script src="filtre.js"></script>
 </head>
 <body>
 <div class="half-background">
@@ -206,7 +204,7 @@ function tempsEcouleDepuisPublication($offre){
     
 
     <div class="filter-menu" id="filterMenu">
-        <button id="closeMenu" class="close-btn">&times;</button>
+        <button id="closeMenu" class="close-btn">&times; </button>
         <h2>Filtres</h2>
         <form>
             <label for="category">Catégorie</label>
