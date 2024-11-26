af94918c8de9fbc5c316a7dfdc2463d341483638
anprieur <antoine.prieur@yahoo.fr>
2024-11-26 16:58:44 +0100

    Update modif_mdp_membre.php

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -136,7 +136,7 @@ if (isset($_POST['modif_infos'])){
             </fieldset>
            
             <div class="compte_membre_save_delete">
-                <button type="submit" class="submit-btn2">Enregistrer</button>
+                <button type="submit" name="modif_infos" class="submit-btn2">Enregistrer</button>
             </div>
         </form>
        
