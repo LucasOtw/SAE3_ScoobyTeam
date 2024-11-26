240b26766826f63c7564f6359feff6db2d77aaa0
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-11-26 10:53:34 +0100

    Update creation_offre_attraction_2.php

--- html/etape_2_horaires/creation_offre_attraction_2.php
+++ html/etape_2_horaires/creation_offre_attraction_2.php
@@ -23,7 +23,7 @@
     </header>
      <div class="fleche_retour">
         <div>
-            <a href="../etape_1_form/creation_offre_attraction1.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
+            <a href="../etape_1_form/creation_offre_attraction_1.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
         </div>
     </div>
 
