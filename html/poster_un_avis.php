7f57ae61ef8d8234d24d4eb8dc75369811ab6ebe
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-11-27 18:54:58 +0100

    Update poster_un_avis.php

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -174,10 +174,10 @@ echo "</pre>";
             $creerAvis->bindParam(':code_compte', $code_compte);
          
         }
-echo $texte_avis;
-echo $note;
-echo $code_offre;
-echo $code_compte;
+echo "Avis : " . $texte_avis;
+echo "Note : " .$note;
+echo "code offre : " . $code_offre;
+echo "code compte : " . $code_compte;
            foreach($erreurs as $erreur){
               echo $erreur;
            }
