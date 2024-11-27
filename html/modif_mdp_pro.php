a9505a6d61c9076dcf84a53ed55d1a9674881d77
anprieur <antoine.prieur@yahoo.fr>
2024-11-27 11:08:49 +0100

    Update modif_mdp_pro.php

--- html/modif_mdp_pro.php
+++ html/modif_mdp_pro.php
@@ -116,7 +116,7 @@ if (isset($_POST['modif_infos'])){
             </ul>
         </section>
 
-        <form action="modif_mdp_pro.php" method="POST">
+        <form action="main_modif_mdp_pro" method="POST">
            <h3>Modifiez votre mot de passe</h3>
            
             <fieldset>
