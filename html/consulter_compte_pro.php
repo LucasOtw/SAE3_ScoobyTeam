02f64f8963687b71e77cc8f94526b4ffe8780c85
anprieur <antoine.prieur@yahoo.fr>
2024-11-27 10:11:08 +0100

    Update consulter_compte_pro.php

--- html/consulter_compte_pro.php
+++ html/consulter_compte_pro.php
@@ -100,7 +100,6 @@ if (isset($_POST['modif_infos'])){
                 <li><a href="mes_offres.php">Accueil</a></li>
                 <li><a href="creation_offre.php">Publier</a></li>
                 <li><a href="consulter_compte_pro.php" class="active">Mon Compte</a></li>
-                <li><a href="modif_mdp_pro.php" class="active">Mot de passe et sécurité</a></li>
             </ul>
         </nav>
     </header>
@@ -122,6 +121,7 @@ if (isset($_POST['modif_infos'])){
                 <li><a href="consulter_compte_pro.php" class="active">Informations personnelles</a></li>
                 <li><a href="mes_offres.php" >Mes offres</a></li>
                 <li><a href="modif_bancaire.php">Compte bancaire</a></li>
+                <li><a href="modif_mdp_pro.php" class="active">Mot de passe et sécurité</a></li>
             </ul>
         </section>
 
