e98d91477eb3f6b19a7d24c830ff87e849abeac8
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-11-27 18:18:53 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -791,6 +791,7 @@
                 style="border:0;margin: auto; width:85vw; height:50vh" allowfullscreen="" loading="lazy">
                 </iframe>
             </div>
+                    
              <div class="avis-widget">
               <div class="avis-header">
                 <h1 class ="avis">5.0 <span class="avis-score">Très bien</span></h1>
