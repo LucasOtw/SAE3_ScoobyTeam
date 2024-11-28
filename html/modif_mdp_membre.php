209111277b69043f9889eade280074b992436c2a
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2024-11-28 11:20:33 +0100

    Ajout

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -132,7 +132,7 @@ if (isset($_POST['modif_infos'])){
             <ul>
                 <li><a href="consulter_compte_membre.php">Informations personnelles</a></li>
                 <li><a href="#" class="active">Mot de passe et sécurité</a></li>
-                <li><a href="#">Historique</a></li>
+                <li><a href="consulter_mes_avis.php">Historique</a></li>
 <!--                     <li><a href="historique_membre.php">Historique</a></li> -->
             </ul>
         </section>
