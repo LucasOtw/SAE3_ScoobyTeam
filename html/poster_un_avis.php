0b348cd6c4572f77450c06a2fed341fe592c70f4
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-11-28 10:04:44 +0100

    Update poster_un_avis.php

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -85,7 +85,7 @@ echo "</pre>";
                     <button class="poster_un_avis_btn_offre">Voir l'offre →</button>
                 </div>
                 <div class="poster_un_avis_images">
-                         <img src="<?php echo $image_offre[0]; ?>" alt=""> 
+                         <img src="<?php echo $image_offre[0]; ?>" alt=""  class="poster_un_avis_image"> 
 <!--                     <img src="images/tiallannec1.png" alt="Image 1" class="poster_un_avis_image"> -->
                 </div>
             </div>
