7d4c3463f9712a08c9e49d999c0078bad2cf6f77
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-11-28 09:30:37 +0100

    Update creation_offre3.php

--- html/creation_offre3.php
+++ html/creation_offre3.php
@@ -147,6 +147,7 @@ if(isset($_POST['valider'])){
 
             $nom_doss = str_replace(' ','',$_SESSION['crea_offre']);
             $chemin = "./images/offres/{nom_doss}";
+            echo $chemin;
 
             if(file_exists($chemin)){
                 echo "ta mère";
