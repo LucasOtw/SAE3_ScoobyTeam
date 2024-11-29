0a146e39a792eb7dffe45d5243811154abfa1c54
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-11-29 15:03:43 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -877,7 +877,6 @@ if (isset($json['results'][0])) {
                 <p class="avis"><?php echo $nombre_d_avis ; ?> avis vérifié<?php if ($nombre_d_avis > 1){ echo 's'; } ?></p>
             </div>
             <div class="avis-list">
-                <div class="avis">
                     <?php
                     foreach ($tout_les_avis as $avis) {
                         $appreciation = "";
@@ -907,11 +906,13 @@ if (isset($json['results'][0])) {
                                 break;
                         }
                         ?>
-                        <div class="avis-content">
-                            <h3 class="avis"><?php echo $avis["note"] . ".0 $appreciation ";?>| <span class="nom_avis"><?php echo $avis["prenom"];?> <?php echo $avis["nom"]; ?></span></h3>
-                            <p class="avis"><?php echo $avis["txt_avis"]; ?></p>
+                        <div class="avis">
+
+                            <div class="avis-content">
+                                <h3 class="avis"><?php echo $avis["note"] . ".0 $appreciation ";?>| <span class="nom_avis"><?php echo $avis["prenom"];?> <?php echo $avis["nom"]; ?></span></h3>
+                                <p class="avis"><?php echo $avis["txt_avis"]; ?></p>
+                            </div>
                         </div>
-                    </div>
                         <?php
                     }
                         ?>            
