67ed298b3b06485bf89ae54fcf4c8d10febf690a
anprieur <antoine.prieur@yahoo.fr>
2024-11-29 09:19:19 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -513,14 +513,14 @@ function tempsEcouleDepuisPublication($offre){
                         offers.sort((a, b) => {
                             const priceA = parseFloat(a.getAttribute('data-price'));
                             const priceB = parseFloat(b.getAttribute('data-price'));
-                            return priceOrder === 'Croissant' ? priceA - priceB : priceB - priceA;
+                            return priceOrder === 'croissantP' ? priceA - priceB : priceB - priceA;
                         });
                     }
                     if (noteOrder) {
                         offers.sort((a, b) => {
                             const noteA = parseFloat(a.getAttribute('data-note'));
                             const noteB = parseFloat(b.getAttribute('data-note'));
-                            return noteOrder === 'Croissant' ? noteA - noteB : noteB - noteA;
+                            return noteOrder === 'croissantN' ? noteA - noteB : noteB - noteA;
                         });
                     }
             
