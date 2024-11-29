c02551785930c542f7cc57494c9c7b9b3b83b2cc
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-11-29 12:02:40 +0100

    Update consulter_mes_avis.php

--- html/consulter_mes_avis.php
+++ html/consulter_mes_avis.php
@@ -97,6 +97,8 @@ if (!empty($_POST['supprAvis'])){
 
                 <?php
 
+                echo $compte['code_compte'];
+
                 $tout_les_avis = $dbh->prepare('SELECT * FROM tripenarvor._avis NATURAL JOIN tripenarvor.membre NATURAL JOIN tripenarvor._offre WHERE code_compte = :code_compte');
 
                 $tout_les_avis->bindValue(':code_compte', $compte['code_compte'], PDO::PARAM_INT);
