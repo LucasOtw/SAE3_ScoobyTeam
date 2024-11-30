24134563d8d6d3f76bf80ca670f29d7c93c65cb2
vasamson <samson.valentin2005@gmail.com>
2024-11-30 11:18:26 +0100

    Add files via upload

---
+++ html/telecharger_facture.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <tit>Télécharger une facture</title>
+    <link rel="stylesheet" href="telecharger_facture.css">
+</head>
+<body>
+<header>
+    <div class="logo">
+        <img src="images/logo_blanc_pro.png" alt="PACT Logo">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="mes_offres.php">Accueil</a></li>
+            <li><a href="connexion_pro.php">Publier</a></li>
+            <li><a href="connexion_pro.pro" class="active">Mon Compte</a></li>
+        </ul>
+    </nav>
+</header>
+<main class="main-ajout-bancaire">
+        <section class="profile">
+            <div class="banner">
+                <img src="images/Fond.png" alt="Bannière de profil">
+            </div>
+            <div class="profile-info">
+                <img class="profile-picture" src="images/hotel.jpg" alt="Profil utilisateur">
+                <h1><?php echo $monCompte['raison_sociale']; ?></h1>
+                <p><?php echo $compte['mail'] . " | " . $compte['telephone']; ?></p>
+            </div>
+        </section>
+
+        <section class="tabs">
+            <ul>
+                <li><a href="consulter_compte_pro.php">Informations personnelles</a></li>
+                <li><a href="mes_offres.php">Mes offres</a></li>
+                <li><a href="#" class="active">Compte bancaire</a></li>
+            </ul>
+        </section>
+
+
+</main>
+<footer class="footer">
+    <div class="footer-links">
+        <div class="logo">
+            <img src="images/logoBlanc.png" alt="Logo PACT">
+        </div>
+        <div class="link-group">
+            <ul>
+                <li><a href="#">Mentions Légales</a></li>
+                <li><a href="#">RGPD</a></li>
+                <li><a href="#">Nous connaître</a></li>
+                <li><a href="#">Nos partenaires</a></li>
+            </ul>
+        </div>
+        <div class="link-group">
+            <ul>
+                <li><a href="mes_offres.php">Accueil</a></li>
+                <li><a href="connexion_pro.php">Publier</a></li>
+                <li><a href="#">Historique</a></li>
+            </ul>
+        </div>
+        <div class="link-group">
+            <ul>
+                <li><a href="#">CGU</a></li>
+                <li><a href="#">Signaler un problème</a></li>
+                <li><a href="#">Nous contacter</a></li>
+            </ul>
+        </div>
+        <div class="link-group">
+            <ul>
+                <li><a href="#">Presse</a></li>
+                <li><a href="#">Newsletter</a></li>
+                <li><a href="#">Notre équipe</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="social-icons">
+            <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
+            <a href="#"><img src="images/Vector2.png" alt="Instagram"></a>
+            <a href="#"><img src="images/youtube.png" alt="YouTube"></a>
+            <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
