ceaead9bb41c5c25cf880e52ae87bcc7c66ee579
anprieur <antoine.prieur@yahoo.fr>
2024-12-01 15:57:42 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -763,7 +763,7 @@ function tempsEcouleDepuisPublication($offre){
                     offerItems.forEach(offer => {
                         
                         const offerEvent = offer.getAttribute('data-event');
-                        console.log(date.' '.offerEvent);
+                        console.log(date+' '+offerEvent);
                         if (!date || date === offerEvent) {
                             offer.style.removeProperty('display');
                         } else {
