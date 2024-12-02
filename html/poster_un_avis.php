6bc080cef9f3906ef69060c5276b596dcdae4b36
quguen <quguen@pce-test>
2024-12-02 13:11:50 +0100

    Modif css creation membre

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -146,7 +146,7 @@ echo "</pre>";
             <a href="voir_offres.php"><img src="images/icones/House icon.png" alt="image de maison"></a>
             <a href="#"><img src="images/icones/Recent icon.png" alt="image d'horloge"></a>
             <a href="#"><img src="images/icones/Croix icon.png" alt="image de PLUS"></a>
-            <a href="r
+            <a href="
                 <?php
                     if(isset($_SESSION["membre"]) || !empty($_SESSION["membre"])){
                         echo "consulter_compte_membre.php";
