a01848ff893fa79494fa54c2f90018dde7b0cb80
quguen <quguen@pce-test>
2024-12-02 09:52:55 +0100

    Ajout ajout_bancaire.css dans styles.css

--- html/ajout_bancaire.php
+++ html/ajout_bancaire.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ajout coordonnées bancaires</title>
-    <link rel="stylesheet" href="ajout_bancaire.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <header>
