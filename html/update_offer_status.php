9a1574b38d90e1ccd1a627321aad72a9257287fb
anprieur <antoine.prieur@yahoo.fr>
2024-12-02 17:08:26 +0100

    Update update_offer_status.php

--- html/update_offer_status.php
+++ html/update_offer_status.php
@@ -17,10 +17,10 @@
     echo"</pre>";
 
     // Récupérer les données envoyées en POST
-    $en_ligne = isset($_POST['en_ligne'])) : null;
+    $en_ligne = isset($_POST['en_ligne']) : null;
     
     // Vérifier que l'identifiant de l'offre est défini
-    $code_offre = isset($_POST['code_offre'])) : null;
+    $code_offre = isset($_POST['code_offre']) : null;
 
     if ($code_offre === null) {
         http_response_code(400);
