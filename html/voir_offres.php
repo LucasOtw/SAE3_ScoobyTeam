2088bc2ee2a67b69a17d4543f81d4ac0ce210ed5
anprieur <antoine.prieur@yahoo.fr>
2024-12-02 13:52:41 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -829,11 +829,11 @@ function tempsEcouleDepuisPublication($offre){
                         {
                             console.log("Boucle n2 : ok\n");
                         }
-                        if ( startDate <= offerPeriodEnd && startDate >= offerPeridoStart)
+                        if ( startDate <= offerPeriodEnd && startDate >= offerPeriodStart)
                         {
                             console.log("Boucle n3 : ok\n");
                         }
-                        if ( endDate >= offerPeriodStart && endDate <= offerPeridoEnd)
+                        if ( endDate >= offerPeriodStart && endDate <= offerPeriodEnd)
                         {
                             console.log("Boucle n4 : ok\n");
                         }
