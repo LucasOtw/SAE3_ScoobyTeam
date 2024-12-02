c86cde0d1b5c217eadb70491eabd8e8e7d79e2e6
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-12-02 16:43:42 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -275,20 +275,6 @@ if (isset($_POST['modif_infos'])){
             <img src="images/icones/User icon.png" alt="image de Personne"></a>
     </nav>
     <footer class="footer footer_membre">
-        <!--<div class="newsletter">
-            <div class="newsletter-content">
-                <h2>Inscrivez-vous à notre Newsletter</h2>
-                <p>PACT</p>
-                <p>Redécouvrez la Bretagne !</p>
-                <form class="newsletter-form">
-                    <input type="email" placeholder="Votre adresse mail" required>
-                    <button type="submit">S'inscrire</button>
-                </form>
-            </div>
-            <div class="newsletter-image">
-                <img src="images/Boiteauxlettres.png" alt="Boîte aux lettres">
-            </div>
-        </div> -->
         
         <div class="footer-links">
             <div class="logo">
