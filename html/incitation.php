0b449f1f2140987a85830200cb7aefc0d42aac24
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-12-02 22:10:58 +0100

    Update incitation.php

--- html/incitation.php
+++ html/incitation.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=429, initial-scale=1.0">
-  <title>PACT Professionnel</title>
+  <title>Créer compte pro</title>
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
