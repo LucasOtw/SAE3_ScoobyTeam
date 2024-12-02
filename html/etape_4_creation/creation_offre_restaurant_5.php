0f48f0363d89fad2b6e33f3f5c25064479cf54b5
vasamson <samson.valentin2005@gmail.com>
2024-12-02 13:35:19 +0100

    Update creation_offre_restaurant_5.php

--- html/etape_4_creation/creation_offre_restaurant_5.php
+++ html/etape_4_creation/creation_offre_restaurant_5.php
@@ -463,7 +463,7 @@ if(isset($_POST['valider'])){
           <h1 class="success-message">Votre offre a été créée avec succès !</h1>
           <div class="buttons">
               <a href="../mes_offres.php" class="back-link">Retourner à "Mes offres"</a>
-              <a href="../télécharger_facture.php" class="back-link">Télécharger ma facture</a>
+              <a href="../telecharger_facture.php" class="back-link">Télécharger ma facture</a>
           </div>
           <div class="image-text">
                 <img src="../images/bonhomme_orange.png" alt="Image description" class="image">
