82403292af5e641dd2ad236bb53ce385f6b54d55
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2024-12-02 16:22:24 +0100

    Page '

--- html/incitation.php
+++ html/incitation.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=429, initial-scale=1.0">
   <title>PACT Professionnel</title>
-  <link rel="stylesheet" href="incitation.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   <div class="incitation_container">
