60cc97c7373a4a049d7c77548908ba9631d7330a
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-12-02 22:06:43 +0100

    Update creation_offre3.php

--- html/creation_offre3.php
+++ html/creation_offre3.php
@@ -374,7 +374,7 @@ if(isset($_POST['valider'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier coordonnées bancaires</title>
+    <title>Création Offre</title>
     <link rel="stylesheet" href="creation_offre3.css">
 </head>
 <body>
