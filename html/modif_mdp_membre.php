fd54dc97550f47a5aac07d3f4813e239fc1fcffe
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2024-12-02 13:59:04 +0100

    Modif headé

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -75,7 +75,7 @@ if (isset($_POST['modif_infos'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Modifier Mot de passe</title>
-    <link rel="stylesheet" href="modif_mdp_membre.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
