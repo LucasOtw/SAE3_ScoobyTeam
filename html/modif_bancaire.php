c94c6cfa9add1e7cb2da8cce569f36f287c9bae4
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-12-03 13:58:54 +0100

    Update modif_bancaire.php

--- html/modif_bancaire.php
+++ html/modif_bancaire.php
@@ -12,6 +12,7 @@ include("recupInfosCompte.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <title>Modifier coordonnées bancaires</title>
     <link rel="stylesheet" href="styles.css">
     
