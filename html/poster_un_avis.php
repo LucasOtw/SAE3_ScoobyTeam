54d4d49be2baec9ab2dee7a4f8d4e89cd9533ac9
vasamson <samson.valentin2005@gmail.com>
2024-12-03 12:52:18 +0100

    Update poster_un_avis.php

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -47,6 +47,7 @@ echo "</pre>";
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Poster un avis</title>
     <link rel="stylesheet" href="styles.css">
