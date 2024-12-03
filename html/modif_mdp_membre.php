a3dbd7db9c60d9d7a3dc6f29edf7f7db58b8b00d
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-12-03 14:50:54 +0100

    Update modif_mdp_membre.php

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -117,7 +117,7 @@ if (isset($_POST['modif_infos'])){
             </div>
 
             <div class="profile-info">
-                <img src="images/icones/icone_compte.png" alt="Photo de profil" class="profile-img">
+                <img src=<?php echo $compte_pp; ?> alt="Photo de profil" class="profile-img">
                 <h1><?php echo $monCompteMembre['prenom']." ".$monCompteMembre['nom']." (".$monCompteMembre['pseudo'].")"; ?></h1>
                 <p><?php echo $compte['mail']; ?> | <?php echo trim(preg_replace('/(\d{2})/', '$1 ', $compte['telephone'])); ?></p>
             </div>
