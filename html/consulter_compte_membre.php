08515d2390ba5100348f338b22081b4b3dd22661
vasamson <samson.valentin2005@gmail.com>
2024-12-03 12:47:37 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -128,6 +128,7 @@ if (isset($_POST['modif_infos'])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualiser Profil</title>
     <link rel="stylesheet" href="styles.css">
