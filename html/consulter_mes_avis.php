26a5500ef95723a246fee4e0896415b3ea4637ec
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-12-03 12:41:28 +0100

    Update consulter_mes_avis.php

--- html/consulter_mes_avis.php
+++ html/consulter_mes_avis.php
@@ -86,7 +86,7 @@ if (!empty($_POST['supprAvis'])){
 
        <header class="header-tel">
             <div class="logo-tel">
-                <img src="images/LogoCouleur.png" alt="PACT Logo">
+                <img src="images/LogoNOoirVert.png" alt="PACT Logo">
             </div>
             
         </header>
