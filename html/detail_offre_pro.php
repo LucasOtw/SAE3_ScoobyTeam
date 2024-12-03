1488405a781f6193993ee484ce966fda425a8025
vasamson <samson.valentin2005@gmail.com>
2024-12-03 12:50:06 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -257,6 +257,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/logoPin_orange.png" width="16px" height="32px">
     <title>Détail offre</title>
     <link rel="stylesheet" href="detail_offre_pro.css?">
     <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script> <!-- Pour les icones -->
