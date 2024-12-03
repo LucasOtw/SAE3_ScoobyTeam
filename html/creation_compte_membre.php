d10984fb3307215e7acac761845544c4ce4a1fc8
vasamson <samson.valentin2005@gmail.com>
2024-12-03 12:48:30 +0100

    Update creation_compte_membre.php

--- html/creation_compte_membre.php
+++ html/creation_compte_membre.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Créer un compte</title>
-    <link rel="icon" type="image/png" href="images/logoPin.png" width="16px" height="32px">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
