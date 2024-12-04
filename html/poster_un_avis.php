820004560ae752972230ecdaa5a6dd6d948aeff5
QuentinUg <152272314+QuentinUguen29@users.noreply.github.com>
2024-12-04 12:29:53 +0100

    Update poster_un_avis.php

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -137,7 +137,7 @@ echo "</pre>";
                        </div>
                        <p class="poster_un_avis_disclaimer">En publiant votre avis, vous acceptez les conditions générales d'utilisation (CGU).</p>
                        <div class="poster_un_avis_buttons">
-                           <button class="poster_un_avis_btn_annuler">Annuler</button
+                           <!--<button class="poster_un_avis_btn_annuler">Annuler</button>-->
                            <button class="poster_un_avis_btn_publier" type="submit">Publier →</button>
                            <input type="hidden" name="uneOffre" value="<?php echo htmlspecialchars(serialize($details_offre)); ?>">
                        </div>
