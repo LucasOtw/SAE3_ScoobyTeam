a4207d76f24089984dddc5bd52b09f6e35a59cb9
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2024-12-04 12:17:19 +0100

    Update creation_offre_restaurant_5.php

--- html/etape_4_creation/creation_offre_restaurant_5.php
+++ html/etape_4_creation/creation_offre_restaurant_5.php
@@ -62,6 +62,10 @@ if($monComptePro['code_compte_bancaire']){
     $infosCB = $recupInfosCB->fetch(PDO::FETCH_ASSOC);
 }
 
+if(isset($_POST['creer_offre_gratuite'])){
+    echo "TA MERE";
+}
+
 if(isset($_POST['valider']) || isset($_POST['passer_cb']) || isset($_POST['creer_offre_gratuite'])){
 
     if(!isset($_POST['passer_cb']) || !isset($_POST['creer_offre_gratuite'])){
