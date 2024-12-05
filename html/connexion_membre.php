9ac84b1c5803151a21bdd457d9b2db9be50c0c29
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2024-12-05 11:12:09 +0100

    Update connexion_membre.php

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -184,6 +184,20 @@ if(!empty($_POST)){
             </div>
         </div>
     </main>
+                                <nav class="nav-bar">
+        <a href="voir_offres.php"><img src="images/icones/House icon.png" alt="image de maison"></a>
+        <a href="consulter_mes_avis.php"><img src="images/icones/Recent icon.png" alt="image d'horloge"></a>
+        <a href="incitation.php"><img src="images/icones/Croix icon.png" alt="image de PLUS"></a>
+        <a href="
+                <?php
+                if (isset($_SESSION["membre"]) || !empty($_SESSION["membre"])) {
+                    echo "compte_membre_tel.php";
+                } else {
+                    echo "connexion_membre.php";
+                }
+                ?>">
+            <img src="images/icones/User icon.png" alt="image de Personne"></a>
+    </nav>
 </body>
 
 </html>
