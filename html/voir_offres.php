78cb8f7daaa071b6987e0d9b628eb1362bf27e8b
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-07 09:35:12 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -167,7 +167,7 @@ function tempsEcouleDepuisPublication($offre){
                         if(isset($_SESSION["membre"]) || !empty($_SESSION["membre"])){
                            ?>
                            <li>
-                               <a href="consulter_compte_membre.php"><?php echo "Mon Compte" /*$compte['nom'] . substr($compte['prenom'], 0, 0)*/;?> </a>
+                               <a href="consulter_compte_membre.php"><?php echo $compte['nom'] . substr($compte['prenom'], 0, 0);?> </a>
                            </li>
                             <?php
                         } else {
