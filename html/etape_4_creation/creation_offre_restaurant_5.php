8de0c35ecaecb20ec4fb77664646a2fdca8385dc
parzival <152271525+Parzival35@users.noreply.github.com>
2025-01-07 16:53:46 +0100

    Update creation_offre_restaurant_5.php

--- html/etape_4_creation/creation_offre_restaurant_5.php
+++ html/etape_4_creation/creation_offre_restaurant_5.php
@@ -383,7 +383,7 @@ if(isset($_POST['valider']) || isset($_POST['passer_cb']) || isset($_POST['creer
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Offre - Restaurant</title>
-    <link rel="stylesheet" href="creation_offre3.css?">
+    <link rel="stylesheet" href="../creation_offre3.css?">
 </head>
 <body>
     <header class="header_pro">
