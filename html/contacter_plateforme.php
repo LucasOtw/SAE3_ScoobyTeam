db16a97dfc459797c39f385b9ab5e09d25582ba2
vasamson <samson.valentin2005@gmail.com>
2025-01-07 14:23:03 +0100

    Update contacter_plateforme.php

--- html/contacter_plateforme.php
+++ html/contacter_plateforme.php
@@ -1,56 +1,40 @@
+
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aide</title>
-    <link rel="stylesheet" href="contacter_plateforme.css">
+    <title>Se connecter</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-    <!-- Header incluant le menu de navigation -->
     <header class="header-pc header_membre">
         <div class="logo-pc">
             <a href="voir_offres.php">
-                <img src="images/logoBlanc.png" alt="PACT Logo">
+                    <img src="images/logoBlanc.png" alt="PACT Logo">
             </a>
         </div>
+       
+        
         <nav>
             <ul>
-                <li><a href="voir_offres.php">Accueil</a></li>
+                <li><a href="voir_offres.php" >Accueil</a></li>
                 <li><a href="connexion_pro.php">Publier</a></li>
-                <li><a href="aide.php" class="active">Aide</a></li>
+                <li><a href="#" class="active">Se connecter</a></li>
             </ul>
         </nav>
     </header>
-
-  
-    <main class="connexion_membre_main">
-        <h2>Nous contacter</h2>
-        <p>Si vous avez des questions, veuillez nous envoyer un message via le formulaire ci-dessous.</p>
-
-        <!-- Formulaire pour poser une question -->
-        <form action="aide.php" method="POST">
-            <fieldset>
-                <legend>Email</legend>
-                <input type="email" name="email" placeholder="Votre adresse e-mail" required>
-            </fieldset>
-            <fieldset>
-                <legend>Votre question</legend>
-                <textarea name="question" placeholder="Posez votre question ici..." required></textarea>
-            </fieldset>
-            <button type="submit">Envoyer</button>
-        </form>
-
-        <!-- Message de confirmation ou d'erreur -->
-        <?php if (isset($message_sent)): ?>
-            <p><?= $message_sent; ?></p>
-        <?php endif; ?>
+    <header class="header-tel header_membre">
+        <div class="logo-tel">
+            <img src="images/LogoCouleur.png" alt="PACT Logo">
+        </div>
+         <a class="btn_plus_tard" href="voir_offres.php">Plus tard</a>
+    </header>
+    <main>    
     </main>
-
-    <!-- Footer -->
-    <footer>
-        <p>&copy; 2025 VotreSite. Tous droits réservés.</p>
-    </footer>
 </body>
-</html>
 
+</html>
