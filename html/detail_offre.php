031da340426a0316597deb059f9daa77a909439f
anprieur <antoine.prieur@yahoo.fr>
2025-01-07 17:04:41 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -698,11 +698,13 @@ if (isset($json['results'][0])) {
         <div class="avis">
             <div class="avis-content">
                 <h3 class="avis">
+                    
                     <?php echo $avis["note"] . ".0 $appreciation "; ?> | <span class="nom_avis"><?php echo $avis["prenom"] . " " . $avis["nom"]; ?></span>
+                
                     <span class="signalement">
-                    <a href="signalement.php?id_avis=<?php echo isset($avis['code_avis']) ? htmlspecialchars($avis['code_avis']) : 'invalide'; ?>" title="Signaler cet avis" style="margin-left: 63vw; text-decoration: none">🚩</a>
-
+                        <a href="signalement.php?id_avis=<?php echo isset($avis['code_avis']) ? htmlspecialchars($avis['code_avis']) : 'invalide'; ?>" title="Signaler cet avis" style="margin-left: 63vw; text-decoration: none">🚩</a>
                     </span>
+                    
                 </h3>
                 <p class="avis"><?php echo $avis["txt_avis"]; ?></p>
             </div>
