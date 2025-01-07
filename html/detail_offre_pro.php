e8d030b5b7b56d1ec076261e355881b1288756ae
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-07 13:49:06 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -380,6 +380,13 @@
                             <div class="slider-circle"></div>
                         </div>
                     </div>
+                     <div> 
+                        <p id="offer-state">L'offre est actuellement : 
+                            <span id="offer-status"> 
+                                <?php echo $details_offre[ 'en_ligne'] ? "En Ligne" : "Hors Ligne"; ?>
+                            </span>
+                        </p>
+                    </div>
 
                     <!-- <script src="toggle-button.js"></script> -->
                     
