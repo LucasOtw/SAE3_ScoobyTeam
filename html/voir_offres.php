3839d2e4f28cbe35fd92781a0c52215700ce3621
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-07 11:00:20 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -97,7 +97,7 @@ function tempsEcouleDepuisPublication($offre){
 <div class="half-background">
         <!-- Le contenu de la page ici -->
          <div class="conteneur_titre_voir_offre">
-        <h6>Bonjour <?php echo $resultat['prenom'];?></h6>
+        <h2>Bonjour <?php echo $resultat['prenom'];?></h2>
         <h1 class="h1_voir_offre1">Découvrez la</h1>
         <h1 class="h1_voir_offre2">Bretagne !</h1>
 </div>
