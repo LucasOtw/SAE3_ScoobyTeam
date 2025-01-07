da1615e2e5de72c6e86cd4dcafa0c3dadbf18226
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-07 09:29:10 +0100

    AJout

--- html/signalement.php
+++ html/signalement.php
@@ -1,9 +1,61 @@
 <?php 
 
-
-
-
-
-
+ob_start();
+session_start();
 
 ?>
+
+<<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les Offres PACT</title>
+    <link rel="stylesheet" href="voir_offres.css">
+    <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
+</head>
+<body>
+<div class="header-membre">
+        <header class="header-pc">
+            <div class="logo-pc" style="z-index: 1">
+                <a href="voir_offres.php">
+                    <img src="images/logoBlanc.png" alt="PACT Logo">
+                </a>
+
+            </div>
+            
+            <nav>
+                <ul>
+                    <li><a href="voir_offres.php" class="active">Accueil</a></li>
+                    <li><a href="connexion_pro.php">Publier</a></li>
+                    <?php
+                        if(isset($_SESSION["membre"]) || !empty($_SESSION["membre"])){
+                           ?>
+                           <li>
+                               <a href="consulter_compte_membre.php"><?php echo "Mon Compte" /*$compte['nom'] . substr($compte['prenom'], 0, 0)*/;?> </a>
+                           </li>
+                            <?php
+                        } else {
+                            ?>
+                           <li>
+                               <a href="connexion_membre.php">Se connecter</a>
+                           </li>
+                           <?php
+                        }
+                    ?>
+                </ul>
+            </nav>
+        </header>
+        <header class="header-tel">
+            <div class="logo-tel">
+                <img src="images/LogoCouleur.png" alt="PACT Logo">
+            </div>
+            
+        </header>
+    </div>
+</body>
+</html>
