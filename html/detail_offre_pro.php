6bd5b2492c94ad665370ef1ec4bb39326fbb9c1b
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-07 13:54:58 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -321,13 +321,13 @@
 
                     <p class="update"><span class="update-icon">⟳</span> Dernière modification <?php echo tempsEcouleDepuisDerniereModif($details_offre);?></p>
                     
-                    <div> 
-                        <p id="offer-state">L'offre est actuellement : 
-                            <span id="offer-status"> 
-                                <?php echo $details_offre[ 'en_ligne'] ? "En Ligne" : "Hors Ligne"; ?>
-                            </span>
-                        </p>
-                    </div>
+                   // <div> 
+                      //  <p id="offer-state">L'offre est actuellement : 
+                      //      <span id="offer-status"> 
+                      //          <?php echo $details_offre[ 'en_ligne'] ? "En Ligne" : "Hors Ligne"; ?>
+                      //      </span>
+                     //   </p>
+                    //</div>
                     <div> 
                         <p id="offer-state"> 
                             <span id="offer-status"> 
@@ -386,13 +386,6 @@
                         <div class="slider" id="slider-toggle" onclick="toggleSlider()">
                             <div class="slider-circle"></div>
                         </div>
-                    </div>
-                     <div> 
-                        <p id="offer-state"> 
-                            <span id="offer-status"> 
-                                <?php echo $details_offre[ 'en_ligne'] ? "En Ligne" : "Hors Ligne"; ?>
-                            </span>
-                        </p>
                     </div>
 
                     <!-- <script src="toggle-button.js"></script> -->
