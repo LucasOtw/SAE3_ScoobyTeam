38467eda24292eb039c996e0792af76f6f56c0ca
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-09 12:28:05 +0100

    ajout

--- html/obtenir_aide.php
+++ html/obtenir_aide.php
@@ -70,6 +70,67 @@
             </li>
         </ul>
     </div>
+    <footer>
+        <div class="newsletter">
+            <div class="newsletter-content">
+                <h2>Inscrivez-vous à notre Newsletter</h2>
+                <p>PACT</p>
+                <p>découvrez la Bretagne !</p>
+                <form class="newsletter-form">
+                    <input type="email" placeholder="Votre adresse mail" required>
+                    <button type="submit">S'inscrire</button>
+                </form>
+            </div>
+            <div class="newsletter-image">
+                <img src="images/Boiteauxlettres.png" alt="Boîte aux lettres">
+            </div>
+        </div>
+        
+        <div class="footer-links">
+            <div class="logo">
+                <img src="images/logoBlanc.png" alt="Logo PAVCT">
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="mentions_legales.html">Mentions Légales</a></li>
+                    <li><a href="#">RGPD</a></li>
+                    <li><a href="#">Nous connaître</a></li>
+                    <li><a href="#">Nos partenaires</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="voir_offres.php">Accueil</a></li>
+                    <li><a href="connexion_pro.php">Publier</a></li>
+                    <li><a href="#">Historique</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">CGU</a></li>
+                    <li><a href="#">Signaler un problème</a></li>
+                    <li><a href="contacter_plateforme.php">Nous contacter</a></li>
+                </ul>
+            </div>
+            <div class="link-group">
+                <ul>
+                    <li><a href="#">Presse</a></li>
+                    <li><a href="#">Newsletter</a></li>
+                    <li><a href="#">Notre équipe</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <div class="social-icons">
+                <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
+                <a href="#"><img src="images/Vector2.png" alt="Instagram"></a>
+                <a href="#"><img src="images/youtube.png" alt="YouTube"></a>
+                <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
 
