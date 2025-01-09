eacd3b8a495a4bc59adef282719a692aa5a3e977
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-09 11:41:28 +0100

    ajout

--- html/obtenir_aide.php
+++ html/obtenir_aide.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obtenir de l'aide</title>
+    <link rel="stylesheet" href="obtenir_aide.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800&display=swap" rel="stylesheet">
 </head>
 <body>
 <header class="header-pc header_membre">
