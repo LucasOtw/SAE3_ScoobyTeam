04c9bb21a7c6efcd0c06d46c312bc0783319c72b
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-09 11:40:39 +0100

    ajout

--- html/obtenir_aide.php
+++ html/obtenir_aide.php
@@ -6,7 +6,28 @@
     <title>Obtenir de l'aide</title>
 </head>
 <body>
-    <h1>Obtenir de l'aide</h1>
+<header class="header-pc header_membre">
+        <div class="logo-pc">
+           <a href="voir_offres.php">
+            <img src="images/logoBlanc.png" alt="PACT Logo">
+         </a>
+
+        </div>
+        <nav>
+            <ul>
+                <li><a href="voir_offres.php">Accueil</a></li>
+                <li><a href="connexion_pro.php">Publier</a></li>
+                <li><a href="consulter_compte_membre.php" class="active">Mon Compte</a></li>
+            </ul>
+        </nav>
+      </header> 
+
+
+        <header class="header-tel header_membre">
+            <div class="logo-tel">
+                <a href="voir_offres.php"><img src="images/logoNoirVert.png" alt="PACT Logo"></a>
+            </div>
+        </header>
 </body>
 </html>
 
