e777ef436f4d71953e9986a81b94be4107792f84
parzival <152271525+Parzival35@users.noreply.github.com>
2025-01-09 11:53:14 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -314,7 +314,7 @@ function tempsEcouleDepuisPublication($offre){
             <h2 class="titre-a-la-une">A La Une</h2>
     
             <div class="a-la-une-carrousel">
-                <button class="card-scroll-btn card-scroll-btn-left" onclick="scrollcontentLeft()">&#8249;</button>
+                <button class="card-scroll-btn card-scroll-btn-left" onclick="scrollcontentLeft('.a-la-une')">&#8249;</button>
                 <section class="a-la-une">
                 <?php
                     try {
@@ -390,7 +390,7 @@ function tempsEcouleDepuisPublication($offre){
                     }
                 ?>
                 </section>
-                <button class="card-scroll-btn card-scroll-btn-right" onclick="scrollcontentRight()">&#8250;</button>
+                <button class="card-scroll-btn card-scroll-btn-right" onclick="scrollcontentRight('.a-la-une')">&#8250;</button>
             </div>
         </div>
 
@@ -629,7 +629,7 @@ function tempsEcouleDepuisPublication($offre){
             <h2 class="titre-vu-recemment">Vu récemment</h2>
     
             <div class="vu-recemment-carrousel">
-                <button class="card-scroll-btn card-scroll-btn-left" onclick="scrollcontentLeft()">&#8249;</button>
+                <button class="card-scroll-btn card-scroll-btn-left" onclick="scrollcontentLeft('.vu-recemment')">&#8249;</button>
                 <section class="vu-recemment">
                 <?php
                     try {
@@ -705,7 +705,7 @@ function tempsEcouleDepuisPublication($offre){
                     }
                 ?>
                 </section>
-                <button class="card-scroll-btn card-scroll-btn-right" onclick="scrollcontentRight()">&#8250;</button>
+                <button class="card-scroll-btn card-scroll-btn-right" onclick="scrollcontentRight('.vu-recemment')">&#8250;</button>
             </div>
         </div>
 
