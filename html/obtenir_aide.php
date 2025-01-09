0b2c6499398d4217cbd8bef4fee4a0973351fb09
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-09 12:13:28 +0100

    ajout

--- html/obtenir_aide.php
+++ html/obtenir_aide.php
@@ -32,6 +32,15 @@
             </div>
         </header>
         <img src="images/faq.png" alt="Image pour la page des questions/aides" class="image-aide">
+        <div class="faq-search-bar-container">
+        <input type="text" class="faq-search-input" placeholder="Comment peut-on vous aider ?">
+        <button class="faq-search-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="11" cy="11" r="8"></circle>
+                <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+        </button>
+    </div>
 </body>
 </html>
 
