81456099e7955e7dea956c7e4e8eef09c8ce2213
anprieur <antoine.prieur@yahoo.fr>
2025-01-09 11:30:27 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -87,10 +87,9 @@ if(isset($_POST['vueDetails']) || isset($_SESSION['detail_offre'])){
     if (!empty($details_offre)) { // si l'offre existe
 
         if (isset($_SESSION["membre"]) || !empty($_SESSION["membre"])) {
-            // $consulter= $dbh->prepare('insert into tripenarvor._consulte (consulter, date_consultation, code_compte, code_offre)
-            //                             values (TRUE, NOW(), :code_compte, :code_offre);');
-            // $consulter->execute([':code_compte' => , ':code_offre' => $code_offre]);
-            var_dump($_SESSION["membre"]);
+            $consulter= $dbh->prepare('insert into tripenarvor._consulte (consulter, date_consultation, code_compte, code_offre)
+                                        values (TRUE, NOW(), :code_compte, :code_offre);');
+            $consulter->execute([':code_compte' => $_SESSION["membre"]["code_compte"], ':code_offre' => $code_offre]);
         }
         
         // Une offre a forcément au moins une image. 
