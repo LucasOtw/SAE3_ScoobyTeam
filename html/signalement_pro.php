b4019dea80ad8f1e1bab41df93a0c4137d4b9b6a
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-09 10:16:42 +0100

    ajout

--- html/signalement_pro.php
+++ html/signalement_pro.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Obtenir de l'aide</title>
+    <title>Signalement d'un avis PRO</title>
 </head>
 <body>
-    <h1>Obtenir de l'aide</h1>
+    
 </body>
 </html>
-
