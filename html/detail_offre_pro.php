73a2ee6c349354d4a51d63ed1b878a8e5c1ac1a8
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-10 12:52:07 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -821,7 +821,7 @@
                             </div>
                         <?php else: ?>
                             <div class="note_prenom">
-                                <?php echo htmlspecialchars($avis['note']) . '.0' . $appreciation; ?> |
+                                <?php echo htmlspecialchars($avis['note']) . '.0 ' . $appreciation . " "; ?> |
                                 <span
                                     class="nom_avis" style="color:var(<?php echo $color ?>)"><?php echo htmlspecialchars($prenom) . ' ' . htmlspecialchars($nom); ?></span>
                             </div>
