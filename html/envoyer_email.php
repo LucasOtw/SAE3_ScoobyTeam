0c184619686b628a42b709f17e0960187372ae9f
vasamson <samson.valentin2005@gmail.com>
2025-01-10 12:50:14 +0100

    Update envoyer_email.php

--- html/envoyer_email.php
+++ html/envoyer_email.php
@@ -7,13 +7,14 @@ require '../phpmailer/src/Exception.php';
 require '../phpmailer/src/PHPMailer.php';
 require '../phpmailer/src/SMTP.php';
 
+$successMessage = ''; // Initialisation de la variable pour afficher la popup
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
     $nom = htmlspecialchars($_POST['nom'], ENT_QUOTES, 'UTF-8');
     $prenom = htmlspecialchars($_POST['prenom'], ENT_QUOTES, 'UTF-8');
     $theme = htmlspecialchars($_POST['theme'], ENT_QUOTES, 'UTF-8');
     $question = htmlspecialchars($_POST['textAreaAvis'], ENT_QUOTES, 'UTF-8');
-
 
     // Créer une instance de PHPMailer
     $mail = new PHPMailer;
@@ -29,40 +30,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Définir l'expéditeur et le destinataire
     $mail->setFrom('noreply.scoobyteam@gmail.com', 'ScoobyTeam');
-    $mail->addAddress('noreply.scoobyteam@gmail.com' , 'Destinataire');
+    $mail->addAddress('noreply.scoobyteam@gmail.com', 'Destinataire');
 
     // Sujet de l'email
     $mail->Subject = 'Support Plateforme - Nouvelle question soumise';
 
-
     // Construire le contenu de l'e-mail
     $mail->isHTML(true);
 
-    
     // Corps du message (avec toutes les infos)
     $mail->Body = "
-                <h1>Nouvelle question d'un utilisateur</h1>
-                <p>Bonjour cher membre de la Scooby-Team : </p>
-                <p><strong>{$prenom} {$nom}</strong> vous à poser une question.</p
-                <p>Voici le thème de la question: <strong>{$theme}</strong></p>
-                <br>
-                <p>Contenu de la question :</p>
-                <p><strong>{$question}</strong></p>
-                <br>
-                <p>Cordialement,</p>
-                <p><strong>L’équipe ScoobyTeam</strong></p>
-            ";
+        <h1>Nouvelle question d'un utilisateur</h1>
+        <p>Bonjour cher membre de la Scooby-Team :</p>
+        <p><strong>{$prenom} {$nom}</strong> vous a posé une question.</p>
+        <p>Voici le thème de la question: <strong>{$theme}</strong></p>
+        <br>
+        <p>Contenu de la question :</p>
+        <p><strong>{$question}</strong></p>
+        <br>
+        <p>Cordialement,</p>
+        <p><strong>L’équipe ScoobyTeam</strong></p>
+    ";
 
-
- if ($mail->send()) {
+    // Essayer d'envoyer l'email
+    try {
+        if ($mail->send()) {
             $successMessage = 'Le message a été envoyé avec succès !';
         }
     } catch (Exception $e) {
         $successMessage = "Erreur lors de l'envoi du message : {$mail->ErrorInfo}";
     }
+}
 ?>
 
-<!-- Popup JS -->
+<!-- Affichage de la popup JS si le message a été envoyé -->
 <?php if ($successMessage): ?>
     <script>
         alert("<?php echo $successMessage; ?>");
