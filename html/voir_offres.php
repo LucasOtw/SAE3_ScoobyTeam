d5f848c942161a22490eb0290f663f6e414627a3
anprieur <antoine.prieur@yahoo.fr>
2025-01-10 09:22:51 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -676,6 +676,11 @@ function tempsEcouleDepuisPublication($offre){
                         $infosOffre = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 
                         foreach($infosOffre as $offre){
+
+                            echo "<pre>";
+                            var_dump($offre);
+                            echo "</pre>";
+                            
                             // Récupérer la ville
                             $villeOffre = $dbh->prepare('SELECT ville FROM tripenarvor._adresse WHERE code_adresse = :code_adresse');
                             $villeOffre->bindParam(":code_adresse", $offre["code_adresse"]);
