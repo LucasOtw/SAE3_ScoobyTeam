b236f0e74616818e8e806ecc17d11cce82f25a1c
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-11 14:30:36 +0100

    Update poster_reponse_pro.php

--- html/poster_reponse_pro.php
+++ html/poster_reponse_pro.php
@@ -177,21 +177,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </main> 
 
     <footer class="footer footer_pro">
-        <div class="newsletter">
-            <div class="newsletter-content">
-                <h2>Inscrivez-vous à notre Newsletter</h2>
-                <p>PACT</p>
-                <p>Redécouvrez la Bretagne !</p>
-                <form class="newsletter-form">
-                    <input type="email" placeholder="Votre adresse mail" required>
-                    <button type="submit">S'inscrire</button>
-                </form>
-            </div>
-            <div class="newsletter-image">
-                <img src="images/Boiteauxlettres.png" alt="Boîte aux lettres">
-            </div>
-        </div>
-
         <div class="footer-links">
             <div class="logo\avis">
                 <img src="images/logoBlanc.png" alt="Logo PACT">
