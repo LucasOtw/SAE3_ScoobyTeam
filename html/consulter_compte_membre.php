b4142680bfc2ca7889fe1a5fe0468b10e35f0e36
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-11 17:43:20 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -18,6 +18,19 @@ if(isset($_GET['deco'])){
     }
 }
 
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../phpmailer/src/Exception.php';
+require '../phpmailer/src/PHPMailer.php';
+require '../phpmailer/src/SMTP.php';
+
+
+
 if (isset($_POST['modif_infos'])){
     $erreur = [];
     // Récupérer les valeurs initiales (par exemple, depuis la base de données)
@@ -235,6 +248,46 @@ if (isset($_POST['changePhoto'])) {
     exit;
 }
 
+// TELECHARGEMENT DES DONNEES (FORMAT JSON)
+if (isset($_POST['dwl-data'])) {
+    // Récupération des avis
+    $avis = $dbh->prepare("SELECT o.titre_offre, a.txt_avis, a.note FROM tripenarvor._avis a 
+                           JOIN tripenarvor._offre o ON a.code_offre = o.code_offre
+                           WHERE a.code_compte = :code_compte");
+    $avis->bindValue(":code_compte", $compte['code_compte']);
+    $avis->execute();
+    $result = $avis->fetchAll(PDO::FETCH_ASSOC);
+
+    $tab_avis = [];
+    foreach ($result as $res) {
+        if (!array_key_exists($res['titre_offre'], $tab_avis)) {
+            $tab_avis[$res['titre_offre']] = [];
+        }
+        $content = html_entity_decode($res['txt_avis'], ENT_QUOTES, 'UTF-8');
+        $tab_avis[$res['titre_offre']][] = [
+            'content' => $content,
+            'note' => $res['note']
+        ];
+    }
+
+    // Préparation des données JSON
+    $data = [
+        'Nom' => $monCompteMembre['nom'],
+        'Prenom' => $monCompteMembre['prenom'],
+        'Pseudo' => $monCompteMembre['pseudo'],
+        'Email' => $compte['mail'],
+        'Téléphone' => $compte['telephone'],
+        'Liste_Avis' => $tab_avis
+    ];
+    $jsonData = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+    // Envoi des en-têtes pour le téléchargement
+    header('Content-Type: application/json');
+    header('Content-Disposition: attachment; filename="mes_donnees_PACT.json"');
+    header('Content-Length: ' . strlen($jsonData));
+    
+    echo $jsonData;
+}
 ?>
 
 <!DOCTYPE html>
@@ -336,7 +389,8 @@ if (isset($_POST['changePhoto'])) {
 
            
 
-        <form action="envoyer_email2.php" method="POST" id="compteForm">
+        <form action="consulter_compte_membre.php" method="POST" id="compteForm">
+
             <div class="crea_pro_raison_sociale_num_siren">
                 <fieldset>
                     <legend>Nom *</legend>
@@ -459,6 +513,7 @@ if (isset($_POST['changePhoto'])) {
                 </ul>
             </div>
         </div>
+
         <div class="footer-bottom">x
             <div class="social-icons">
                 <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
