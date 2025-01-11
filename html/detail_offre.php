106bd9bd5b2f6dddb42037a80043bad866a6a42a
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-11 13:30:07 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -824,7 +824,7 @@ WHERE code_offre = :code_offre
                 print_r($tous_les_avis);
                 echo "</pre>";
                 foreach ($tous_les_avis as $cle => $avis) {
-                    echo $cle."<br>";
+                    echo "TEST : "$cle."<br>";
                     echo "<pre>";
                     print_r($avis);
                     echo "</pre>";
