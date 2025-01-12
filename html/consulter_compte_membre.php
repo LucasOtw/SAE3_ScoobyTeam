e4db5828f349a0db9eb2d6bd8dc16dbd17e49d1c
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-12 16:24:55 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -431,16 +431,11 @@ if (isset($_POST['dwl-data'])) {
             <div class="compte_membre_save_delete_remove">
                 <div class="supprimer-download">
                     <button type="button" name="suppr-compte" class="btn-suppr-compte" id="btn-suppr-compte">Supprimer le compte</button>
-                    <form action="#" method="POST">
-                        <div>
-                            <input type="submit" name="dwl-data" class="btn-dwl-data" value="Télécharger mes données">
-                        </div>
-                    </form>
+                    <input type="submit" name="dwl-data" class="btn-dwl-data" value="Télécharger mes données">
                 </div>
                 <div class="compte_membre_save_delete">
                     <a href="?deco=true" class="submit-btn1">Déconnexion</a>
                     <button type="submit" name="modif_infos" class="submit-btn2" id="btn-enreg">Enregistrer</button>
-                    <input type="submit" name="dwl-data" class="btn-dwl-data" value="Télécharger mes données">
                 </div>
             </div>
 
