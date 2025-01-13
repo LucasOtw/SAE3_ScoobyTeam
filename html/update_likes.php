202302353eacc8d4259c3935312392071e73f69c
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-13 15:27:46 +0100

    Update update_likes.php

--- html/update_likes.php
+++ html/update_likes.php
@@ -5,13 +5,16 @@ $dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
 $username = "sae";  // Utilisateur PostgreSQL défini dans .env
 $password = "philly-Congo-bry4nt";  // Mot de passe PostgreSQL défini dans .env
 
+var_dump($_POST);
+
+// Vérifier si les données sont bien reçues
+if (!isset($_POST['action']) || !isset($_POST['code_avis'])) {
+    echo 'Erreur: données manquantes';
+    exit;
+}
 // Get data from the AJAX request
 $action = $_POST['action'];
 $codeAvis = (int) $_POST['avis']['code_avis'];
-echo "<pre>";
-var_dump($_SESSION);
-echo "</pre>";
-
 
 if ($action === 'like') {
     $stmt = $dbh->prepare('
