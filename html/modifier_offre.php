63671be0d80397123a594029120349c82c7bf6bd
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-13 14:13:12 +0100

    Update modifier_offre.php

--- html/modifier_offre.php
+++ html/modifier_offre.php
@@ -222,7 +222,7 @@ echo "</pre>";
                     <label for="description">Description (*)</label>
                     <span contenteditable="true" id="description" data-sync="desc_modif"><?php echo $offre['_description'] ?></span>
                     <label for="accessibilite">Accessibilité</label>
-                    <span contenteditable="true" id="accessibilite" data-sync="access_modif"><?php echo "Test"; ?></span>
+                    <span contenteditable="true" placeholder="Accessibilité" id="accessibilite" data-sync="access_modif"><?php echo $offre['accessibilite'] ?? ""; ?></span>
 
                     <input type="hidden" id="resume_modif" name="_resume_modif">
                     <input type="hidden" id="desc_modif" name="_desc_modif">
