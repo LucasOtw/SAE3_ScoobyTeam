6bcd7b9e5036e82f9b8ed47cefcf44d463289f56
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-13 13:17:00 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -830,6 +830,8 @@ if (isset($json['results'][0])) {
                     <!-- Pouce positif -->
                     <span class="pouce">
                         <img id="positiveImage" src="images/pouce_positif_blanc.png" alt="Pouce positif" onclick="togglePositiveImage()">
+                        <?php echo $Avis['pouce-positif'] ? $Avis['pouce-positif'] . " Likes" : "Pas de Likes";?>
+
                     </span>
 
                     <!-- Pouce négatif -->
