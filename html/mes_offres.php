9b421453f6b015720b1724ff751a5753ab96afa2
Lucas Torres <lucas@dy141082.univ-rennes1.fr>
2025-01-13 09:21:47 +0100

    ajout

--- html/mes_offres.php
+++ html/mes_offres.php
@@ -51,7 +51,11 @@ if(isset($_SESSION['aCreeUneOffre'])){
             <li><a href="mes_offres.php" class="active">Accueil</a></li>
             <li><a href="creation_offre.php">Publier</a></li>
             <li><a href="consulter_compte_pro.php">Mon Compte</a></li>
-            <li><a <img src="images/notif.png" alt="cloche notification" class="nouvelle-image"></a></li>
+            <li>
+                <a href="#">
+                    <img src="images/notif.png" alt="cloche notification" class="nouvelle-image">
+                </a>
+            </li>
         </ul>
     </nav>
 </header>
