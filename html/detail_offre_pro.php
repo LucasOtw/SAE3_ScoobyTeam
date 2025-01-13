b6b9053120590bb84852c78b2d56ae15535b1189
anprieur <antoine.prieur@yahoo.fr>
2025-01-13 09:47:28 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -240,7 +240,16 @@ if (isset($_POST['vueDetails']) || isset($_SESSION['detail_offre'])) {
             }
         }
 
-
+        
+        $option_en_relief = $dbh->prepare('SELECT * FROM tripenarvor._option WHERE code_option = :option_en_relief');
+        $option_en_relief->bindValue(":option_en_relief",$details_offre["option_en_relief"]);
+        $option_en_relief->execute();
+        $option_en_relief = $option_en_relief→fetch(PDO::FETCH_ASSOC);
+        
+        $option_a_la_une = $dbh->prepare('SELECT * FROM tripenarvor._option WHERE code_option = :option_a_la_une');
+        $option_a_la_une->bindValue(":option_a_la_une",$details_offre["option_a_la_une"]);
+        $option_a_la_une->execute();
+        $option_a_la_une = $option_a_la_une->fetch(PDO::FETCH_ASSOC);
 
 
         // On récupère aussi l'adresse indiquée, ainsi que les horaires (si non nulles)
