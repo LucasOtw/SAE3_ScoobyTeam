c0696814991e1cf4d51e123c132d8cd860934bf6
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-14 11:08:29 +0100

    Update consulter_mes_avis.php

--- html/consulter_mes_avis.php
+++ html/consulter_mes_avis.php
@@ -237,7 +237,7 @@ if (!empty($_POST['supprAvis'])){
                 <ul>
                     <li><a href="voir_offres.php">Accueil</a></li>
                     <li><a href="connexion_pro.php">Publier</a></li>
-                    <li><a href="connexion_memebre.php">Se Connecter</a></li>
+                    <li><a href="consulter_compte_membre">Mon Compte</a></li>
                 </ul>
             </div>
             <div class="link-group">
