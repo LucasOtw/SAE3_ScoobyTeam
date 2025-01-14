6e3d1c18eaa8f4bd3835bca35a4949395a3988c9
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-14 13:02:07 +0100

    Update modifier_offre.php

--- html/modifier_offre.php
+++ html/modifier_offre.php
@@ -529,9 +529,9 @@ if($infos_offre !== null){
                             <input type="time" id="fermeture" name="horaires[<?php echo $jour; ?>]["fermeture"]" placeholder="Fermeture"
                             <?php if(isset($codes_horaires[$jour]['fermeture']) && $codes_horaires[$jour]['fermeture'] !== null){
                                 ?>
-                                value="<?php echo htmlspecialchars($codes_hoarires[$jour]['fermeture']); ?>"
+                                value="<?php echo htmlspecialchars($codes_horaires[$jour]['ouverture']); ?>"
                                 <?php
-                            }  ?>>
+                            } ?>>
                         </fieldset>
                     </div>
                 </div>
