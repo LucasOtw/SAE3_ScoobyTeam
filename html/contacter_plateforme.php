c933089dcc2c363234e7d7589d8df7eb69f0586c
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-14 09:11:55 +0100

    Update contacter_plateforme.php

--- html/contacter_plateforme.php
+++ html/contacter_plateforme.php
@@ -113,11 +113,11 @@
 
         <div class="footer-links">
             <div class="logo\avis">
-                <img src="images/logoBlanc.png" alt="Logo PACT">
+                <img src="images/logoBlanc.png" alt="Logo PAVCT">
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Mentions Légales</a></li>
+                    <li><a href="mentions_legales.html">Mentions Légales</a></li>
                     <li><a href="#">RGPD</a></li>
                     <li><a href="#">Nous connaître</a></li>
                     <li><a href="#">Nos partenaires</a></li>
@@ -125,27 +125,26 @@
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Publier</a></li>
-                    <li><a href="#">Historique</a></li>
+                    <li><a href="voir_offres.php">Accueil</a></li>
+                    <li><a href="connexion_pro.php">Publier</a></li>
+                    <li><a href="connexion_memebre.php">Se Connecter</a></li>
                 </ul>
             </div>
             <div class="link-group">
                 <ul>
                     <li><a href="#">CGU</a></li>
-                    <li><a href="#">Signaler un problème</a></li>
-                    <li><a href="#">Nous contacter</a></li>
+                    <li><a href="contacter_plateforme.php">Signaler un problème</a></li>
+                    <li><a href="contacter_plateforme.php">Nous contacter</a></li>
                 </ul>
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Presse</a></li>
+                    <!--<li><a href="#">Presse</a></li>
                     <li><a href="#">Newsletter</a></li>
-                    <li><a href="#">Notre équipe</a></li>
+                    <li><a href="#">Notre équipe</a></li>-->
                 </ul>
             </div>
         </div>
-
         <div class="footer-bottom">
             <div class="social-icons">
                 <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
