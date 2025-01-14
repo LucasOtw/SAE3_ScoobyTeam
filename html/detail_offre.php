4de6eeac65f2af08b71abf20e15e4f8b6e15d9ad
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-01-14 12:29:35 +0100

    Update detail_offre.php

--- html/detail_offre.php
+++ html/detail_offre.php
@@ -878,12 +878,18 @@ if (isset($json['results'][0])) {
                 
                 <div class="signalement_repondre">
                     <div class="pouce pouce<?php echo $avis['code_avis']; ?>">
-                        <img id="positiveImage<?php echo $avis['code_avis']; ?>" src="images/pouce_positif_blanc.png" alt="Pouce positif" onclick="togglePositiveImage(<?php echo $avis['code_avis']; ?>)">
+                        <!-- Pouce positif -->
+                        <img id="positiveImage<?php echo $avis['code_avis']; ?>"
+                            src="<?php echo $voteState == 1 ? 'images/pouce_positif_couleur.png' : 'images/pouce_positif_blanc.png'; ?>"
+                            alt="Pouce positif" onclick="togglePositiveImage(<?php echo $avis['code_avis']; ?>)">
                         <p><?php echo $avis['pouce_positif']; ?></p>           
                     </div>
-                    
+                
                     <div class="pouce pouce<?php echo $avis['code_avis']; ?>">
-                        <img id="negativeImage<?php echo $avis['code_avis']; ?>" src="images/pouce_negatif_blanc.png" alt="Pouce négatif" onclick="toggleNegativeImage(<?php echo $avis['code_avis']; ?>)">
+                        <!-- Pouce négatif -->
+                        <img id="negativeImage<?php echo $avis['code_avis']; ?>"
+                            src="<?php echo $voteState == -1 ? 'images/pouce_negatif_couleur.png' : 'images/pouce_negatif_blanc.png'; ?>"
+                            alt="Pouce négatif" onclick="toggleNegativeImage(<?php echo $avis['code_avis']; ?>)">
                         <p><?php echo $avis['pouce_negatif']; ?></p>
                     </div>
 
