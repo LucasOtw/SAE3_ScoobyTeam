592b9d9bae65212830310600df18f2153b010443
anprieur <antoine.prieur@yahoo.fr>
2025-01-14 10:07:48 +0100

    Update mes_offres.php

--- html/mes_offres.php
+++ html/mes_offres.php
@@ -78,6 +78,36 @@ if(isset($_SESSION['aCreeUneOffre'])){
                     <span class="new-notif-dot"></span>
                 </div>
             </li>
+            <li>
+                <img src="images/user2.png" alt="photo de profil" class="profile-img">
+                <div class="notification-content">
+                    <strong>Quentin Uguen</strong>
+                    <span class="notification-location"> | Abbaye de Monfort</span>
+                    <p>Déplorable, environnement moche</p>
+                    <span class="notification-time">1 mois</span>
+                    <span class="new-notif-dot"></span>
+                </div>
+            </li>
+            <li>
+                <img src="images/user2.png" alt="photo de profil" class="profile-img">
+                <div class="notification-content">
+                    <strong>Quentin Uguen</strong>
+                    <span class="notification-location"> | Abbaye de Monfort</span>
+                    <p>Déplorable, environnement moche</p>
+                    <span class="notification-time">1 mois</span>
+                    <span class="new-notif-dot"></span>
+                </div>
+            </li>
+            <li>
+                <img src="images/user2.png" alt="photo de profil" class="profile-img">
+                <div class="notification-content">
+                    <strong>Quentin Uguen</strong>
+                    <span class="notification-location"> | Abbaye de Monfort</span>
+                    <p>Déplorable, environnement moche</p>
+                    <span class="notification-time">1 mois</span>
+                    <span class="new-notif-dot"></span>
+                </div>
+            </li>
         </ul>
     </div>
 </li>
