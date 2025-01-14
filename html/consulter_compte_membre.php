1e84b0242d4b2240971d787c153a11ee825d15af
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-14 09:08:53 +0100

    Update consulter_compte_membre.php

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -577,11 +577,11 @@ if (isset($_POST['dwl-data'])) {
         
         <div class="footer-links">
             <div class="logo">
-                <img src="images/logoBlanc.png" alt="Logo PACT">
+                <img src="images/logoBlanc.png" alt="Logo PAVCT">
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Mentions Légales</a></li>
+                    <li><a href="mentions_legales.html">Mentions Légales</a></li>
                     <li><a href="#">RGPD</a></li>
                     <li><a href="#">Nous connaître</a></li>
                     <li><a href="#">Nos partenaires</a></li>
@@ -591,21 +591,21 @@ if (isset($_POST['dwl-data'])) {
                 <ul>
                     <li><a href="voir_offres.php">Accueil</a></li>
                     <li><a href="connexion_pro.php">Publier</a></li>
-                    <li><a href="consulter_mes_avis.php">Historique</a></li>
+                    <li><a href="connexion_memebre.php">Se Connecter</a></li>
                 </ul>
             </div>
             <div class="link-group">
                 <ul>
                     <li><a href="#">CGU</a></li>
-                    <li><a href="#">Signaler un problème</a></li>
-                    <li><a href="#">Nous contacter</a></li>
+                    <li><a href="contacter_plateforme.php">Signaler un problème</a></li>
+                    <li><a href="contacter_plateforme.php">Nous contacter</a></li>
                 </ul>
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">Presse</a></li>
+                    <!--<li><a href="#">Presse</a></li>
                     <li><a href="#">Newsletter</a></li>
-                    <li><a href="#">Notre équipe</a></li>
+                    <li><a href="#">Notre équipe</a></li>-->
                 </ul>
             </div>
         </div>
