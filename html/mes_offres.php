810db65cdf13fcb1f01f15274a12f61b106d341a
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-14 11:03:45 +0100

    Update mes_offres.php

--- html/mes_offres.php
+++ html/mes_offres.php
@@ -242,7 +242,7 @@ function tempsEcouleDepuisPublication($offre){
                 <ul>
                     <li><a href="voir_offres.php">Accueil</a></li>
                     <li><a href="connexion_pro.php">Publier</a></li>
-                    <li><a href="connexion_memebre.php">Se Connecter</a></li>
+                    <li><a href="consulter_compte_pro.php">Mon Compte</a></li>
                 </ul>
             </div>
             <div class="link-group">
