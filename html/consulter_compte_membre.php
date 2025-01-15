16bd57c30d85ab1a5a07b2f445ed0fcec6f0f0aa
Lucas Torres <lucas@mac.home>
2025-01-15 19:06:03 +0100

    ajout

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -434,7 +434,7 @@ if (isset($_POST['dwl-data'])) {
                 <div class="supprimer-download">
                     <!-- Bouton "Supprimer le compte" et bouton pour "Mes données" -->
                     <button type="button" name="suppr-compte" class="btn-suppr-compte" id="btn-suppr-compte">Supprimer le compte</button>
-                    <button type="button" name="mes-donnees" class="submit-btn2" id="btn-mes-donnees">Mes données</button>
+                    <button type="button" name="mes-donnees" class="submit-btn4" id="btn-mes-donnees">Mes données</button>
                 </div>
                 <div style="display: flex; gap:6px">
                     <a href="?deco=true" class="submit-btn1">Déconnexion</a>
