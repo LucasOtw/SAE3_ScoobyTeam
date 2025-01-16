1278688f5b091a023b52d7c0fd3d76df54d82aba
Lucas Torres <lucas@mac.home>
2025-01-16 20:38:51 +0100

    ajout

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -391,14 +391,10 @@ if (isset($_POST['vueDetails']) || isset($_SESSION['detail_offre'])) {
                     
                     <p class="detail_offre_price"><?php echo $details_offre["tarif"];?>€</p>
                     <div class="detail_offre_pro_button">                        
-                    <?php if (!empty($details_offre["site_web"])) { ?> <a href="<?php echo $details_offre["site_web"]; ?>" target="_blank"><button class="visit-button_detailoffre_pro">Voir le site ➔</button></a> <?php } ?>
+                    <?php if (!empty($details_offre["site_web"])) { ?> <a href="<?php echo $details_offre["site_web"]; ?>" target="_blank"><button class="visit-button_detailoffre">Voir le site ➔</button></a> <?php } ?>
                     <form id="add-btn" action="modifier_offre.php" method="POST">
                         <input type="hidden" name="uneOffre" value="<?php echo htmlspecialchars(serialize($details_offre)); ?>">
-                        <input id="btn-voir-offre" class="button-text add-btn" type="submit" name="envoiOffre" value="Modifier votre offre" style="
-    display: flex;
-    justify-content: space-between;
-    gap: 13px;
-">
+                        <input id="btn-voir-offre" class="button-text add-btn" type="submit" name="envoiOffre" value="Modifier votre offre">
                     </form>
 
 
