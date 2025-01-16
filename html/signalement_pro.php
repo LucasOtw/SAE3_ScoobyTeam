0fc3e42175fad27ff65d8d0c38d4da9c5baeae25
Quentin Uguen <quguen@pce-test>
2025-01-16 10:18:09 +0100

    Ajout*

--- html/signalement_pro.php
+++ html/signalement_pro.php
@@ -22,7 +22,7 @@ try {
                 NATURAL JOIN tripenarvor._membre
                 WHERE code_compte=2 AND code_avis = :id
             ");
-            
+
             // Lier le paramètre :id et exécuter la requête
             $stmt->execute([':id' => $idAvis]);
 
@@ -57,19 +57,19 @@ try {
             },
             body: JSON.stringify({ id: avisId })
         })
-        .then(response => response.json()) // Parse la réponse JSON
-        .then(data => {
-            if (data.success) {
-                alert('Avis signalé avec succès.');
-                window.location.reload(); // Recharge la page ou redirigez si nécessaire
-            } else {
-                alert(data.message || 'Erreur lors du signalement de l\'avis.');
-            }
-        })
-        .catch(error => {
-            console.error('Erreur réseau ou serveur :', error);
-            alert('Impossible de signaler l\'avis.');
-        });
+            .then(response => response.json()) // Parse la réponse JSON
+            .then(data => {
+                if (data.success) {
+                    alert('Avis signalé avec succès.');
+                    window.location.reload(); // Recharge la page ou redirigez si nécessaire
+                } else {
+                    alert(data.message || 'Erreur lors du signalement de l\'avis.');
+                }
+            })
+            .catch(error => {
+                console.error('Erreur réseau ou serveur :', error);
+                alert('Impossible de signaler l\'avis.');
+            });
     }
 
     // Fonction pour fermer la modale
@@ -78,7 +78,7 @@ try {
     }
 
     // Ferme la modale si l'utilisateur clique en dehors de la fenêtre de la modale
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         var modal = document.getElementById('confirmationModal');
         if (event.target === modal) {
             modal.style.display = 'none';
@@ -89,6 +89,7 @@ try {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -97,15 +98,18 @@ try {
     <link rel="icon" type="image/png" href="images/logoPin_vert.png" width="16px" height="32px">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+        rel="stylesheet">
     <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
 </head>
+
 <body>
-<header class="header-pc header_pro">
+    <header class="header-pc header_pro">
         <div class="logo">
-           <a href="mes_offres.php">
-            <img src="images/logo_blanc_pro.png" alt="PACT Logo">
-         </a>
+            <a href="mes_offres.php">
+                <img src="images/logo_blanc_pro.png" alt="PACT Logo">
+            </a>
         </div>
         <nav class="nav">
             <ul>
@@ -116,7 +120,7 @@ try {
         </nav>
     </header>
     </div>
-    <div class="container">
+    <div class="container-signalement">
         <h2 class="titre_signalement_1">Signaler un avis</h2>
         <?php if (isset($erreur)): ?>
             <div class="erreur">
@@ -124,40 +128,40 @@ try {
             </div>
         <?php else: ?>
             <div class="avis">
-                <?php 
-                    if ($avis) {
-                        // Afficher les informations de l'avis trouvé
-                        if (htmlspecialchars($avis['note']) == 0)
-                            {
-                                $note_avis='Réponse';
-                            } else {
-                                $note_avis=htmlspecialchars($avis['note']).".0";
-                            }
-                            
-                            echo "<h3>" . $note_avis . " | " . htmlspecialchars($avis['prenom']) . " " . htmlspecialchars($avis['nom']) . "</h3>";
-                            echo "<p>" . $avis['txt_avis'] . "</p>";
+                <?php
+                if ($avis) {
+                    // Afficher les informations de l'avis trouvé
+                    if (htmlspecialchars($avis['note']) == 0) {
+                        $note_avis = 'Réponse';
                     } else {
-                        // Aucun avis trouvé avec cet ID
-                        echo "Aucun avis trouvé pour cet ID.";
+                        $note_avis = htmlspecialchars($avis['note']) . ".0";
                     }
+
+                    echo "<h3>" . $note_avis . " | " . htmlspecialchars($avis['prenom']) . " " . htmlspecialchars($avis['nom']) . "</h3>";
+                    echo "<p>" . $avis['txt_avis'] . "</p>";
+                } else {
+                    // Aucun avis trouvé avec cet ID
+                    echo "Aucun avis trouvé pour cet ID.";
+                }
                 ?>
             </div>
-            
+
         <?php endif; ?>
         <h2 class="titre_signalement_2">Cause du signalement</h2>
         <div class="type_offre_select_button">
-                    <select id="offre" name="offreChoisie" data-placeholder="Sélectionnez..." required>
-                        <option value="" hidden selected>Sélectionnez...</option>
-                        <option value="restaurant">Language déplacé</option>
-                        <option value="spectacle">Harcèlement</option>
-                        <option value="visite">Diffamation</option>
-                        <option value="attraction">Spam</option>
-                        <option value="activite">Autre</option>
-                    </select>
+            <select id="offre" name="offreChoisie" data-placeholder="Sélectionnez..." required>
+                <option value="" hidden selected>Sélectionnez...</option>
+                <option value="restaurant">Language déplacé</option>
+                <option value="spectacle">Harcèlement</option>
+                <option value="visite">Diffamation</option>
+                <option value="attraction">Spam</option>
+                <option value="activite">Autre</option>
+            </select>
 
         </div>
         <h2 class="titre_signalement_3">Description (facultatif)</h2>
-        <textarea placeholder="Écrivez votre avis ici..." class="signaler_un_avis_textarea" name="textAreaAvis" id="textAreaAvis"></textarea>
+        <textarea placeholder="Écrivez votre avis ici..." class="signaler_un_avis_textarea" name="textAreaAvis"
+            id="textAreaAvis"></textarea>
         <form method="POST" action="signalement.php">
             <input type="hidden" name="id_avis" value="<?php echo $idAvis; ?>">
             <button type="submit" onclick="showConfirmation(event)">Confirmer le signalement</button>
@@ -165,61 +169,64 @@ try {
 
     </div>
     <!-- Modale de confirmatiosn -->
-<div id="confirmationModal" class="modal">
-    <div class="modal-content">
-        <span class="close" onclick="closeModal()">&times;</span>
-        <div class="popup-message">
-            <p>Votre signalement a bien été envoyé et pris en compte !</p>
+    <div id="confirmationModal" class="modal">
+        <div class="modal-content">
+            <span class="close" onclick="closeModal()">&times;</span>
+            <div class="popup-message">
+                <p>Votre signalement a bien été envoyé et pris en compte !</p>
+            </div>
         </div>
     </div>
-</div>
 
-<!-- Style pour la modale -->
-<style>
-    /* Styles pour la modale */
-    .modal {
-        display: none; /* Masquer la modale par défaut */
-        position: fixed;
-        z-index: 1;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0,0,0,0.5); /* Fond sombre */
-        overflow: auto;
-        padding-top: 60px;
-    }
+    <!-- Style pour la modale -->
+    <style>
+        /* Styles pour la modale */
+        .modal {
+            display: none;
+            /* Masquer la modale par défaut */
+            position: fixed;
+            z-index: 1;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.5);
+            /* Fond sombre */
+            overflow: auto;
+            padding-top: 60px;
+        }
 
-    .modal-content {
-        background-color: #fff;
-        margin: 5% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 80%;
-        max-width: 500px;
-        border-radius: 10px;
-    }
+        .modal-content {
+            background-color: #fff;
+            margin: 5% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            max-width: 500px;
+            border-radius: 10px;
+        }
 
-    .close {
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-    }
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
 
-    .close:hover,
-    .close:focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
-    }
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
 
-    .popup-message {
-        text-align: center;
-        font-size: 18px;
-        color: green;
-    }
-</style>
+        .popup-message {
+            text-align: center;
+            font-size: 18px;
+            color: green;
+        }
+    </style>
 
 </body>
+
 </html>
