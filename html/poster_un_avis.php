5a9f61586e84efa798faa6a4e4e9df797c4a9fb5
anprieur <antoine.prieur@yahoo.fr>
2025-01-16 11:41:11 +0100

    Update poster_un_avis.php

--- html/poster_un_avis.php
+++ html/poster_un_avis.php
@@ -102,7 +102,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
               }
             }
         } else {
-           echo "Vous ne pouvez pas poster d'avis sans notation ou de message !";
+           ?>
+           <!-- Popup de confirmation -->
+            <div class="custom-confirm" id="customConfirmBox">
+                <div class="custom-confirm-content">
+                    <p>Votre message a été envoyé avec succès !</p>
+                    <button id="confirmButton">Fermer</button>
+                </div>
+            </div>
+           <?php
         }
      }
 }
