635d19b6c53705104034c8daa303748c35f57e48
Joseph MULLIEZ <159727096+jmulliez@users.noreply.github.com>
2025-01-16 10:18:02 +0100

    Update obtenir_aide.php

--- html/obtenir_aide.php
+++ html/obtenir_aide.php
@@ -85,25 +85,39 @@
             <div class="logo">
                 <img src="images/logoBlanc.png" alt="Logo PAVCT">
             </div>
-             <div class="link-group">
+            <div class="link-group">
                 <ul>
                     <li><a href="mentions_legales.html">Mentions Légales</a></li>
-                    <li><a href="#">RGPD</a></li>
-                    <li><a href="#">Nous connaître</a></li>
-                    <li><a href="#">Nos partenaires</a></li>
+                    <li><a href="cgu.html">GGU</a></li>
+                    <li><a href="cgv.html">CGV</a></li>
                 </ul>
             </div>
             <div class="link-group">
                 <ul>
                     <li><a href="voir_offres.php">Accueil</a></li>
                     <li><a href="connexion_pro.php">Publier</a></li>
-                    <li><a href="connexion_memebre.php">Se Connecter</a></li>
+                    <?php
+                    if (isset($_SESSION["membre"]) && !empty($_SESSION["membre"])) {
+                        ?>
+                        <li>
+                            <a href="consulter_compte_membre.php">Mon Compte</a>
+                        </li>
+                        <?php
+                    } else {
+                        ?>
+                        <li>
+                            <a href="connexion_membre.php">Se connecter</a>
+                        </li>
+                        <?php
+                    }
+                    ?>
                 </ul>
+
             </div>
             <div class="link-group">
                 <ul>
-                    <li><a href="#">CGU</a></li>
-                    <li><a href="contacter_plateforme.php">Signaler un problème</a></li>
+                    <li><a href="#">Nous Connaitre</a></li>
+                    <li><a href="obtenir_aide.php">Obtenir de l'aide</a></li>
                     <li><a href="contacter_plateforme.php">Nous contacter</a></li>
                 </ul>
             </div>
@@ -115,7 +129,6 @@
                 </ul>
             </div>
         </div>
-
         <div class="footer-bottom">
             <div class="social-icons">
                 <a href="#"><img src="images/Vector.png" alt="Facebook"></a>
