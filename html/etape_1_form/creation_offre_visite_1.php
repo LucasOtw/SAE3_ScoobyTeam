04d5fe5c4cefc27eef2b84aa11d5a16cdb7d66c2
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-01-16 09:57:01 +0100

    Update creation_offre_visite_1.php

--- html/etape_1_form/creation_offre_visite_1.php
+++ html/etape_1_form/creation_offre_visite_1.php
@@ -380,10 +380,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                     <thead>
                         <tr>
                             <th>
-                                Checkbox
+                                Tags
                             </th>
                             <th>
-                                Tag
                             </th>
                         </tr>
                     </thead>
