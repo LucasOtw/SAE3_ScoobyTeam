778ae536ae92038a9686b8659f4121816f6220fd
anprieur <antoine.prieur@yahoo.fr>
2025-01-23 18:38:55 +0100

    liberation memoire

--- tchatator/bdd.h
+++ tchatator/bdd.h
@@ -2,6 +2,7 @@
 #define BDD_H
 
 #include <libpq-fe.h> // Bibliothèque PostgreSQL
+#include <stdbool.h>
 
 
 // Fonction pour me connecter à la bdd
@@ -9,5 +10,8 @@ PGconn* connect_to_db();
 
 // Fonction qui génère un token aléatoire de 4 caractères et le retourne
 char* generate_token();
+
+// Fonction utilitaire pour exécuter une requête PostgreSQL et vérifier le résultat
+bool execute_query(PGconn *conn, const char *query);
 
 #endif
