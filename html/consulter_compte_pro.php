4af43e27576acb790fabb41670ba080f6a3baedc
Lucas Torres <lucas@dy139124.univ-rennes1.fr>
2025-01-24 08:35:27 +0100

    ajout

--- html/consulter_compte_pro.php
+++ html/consulter_compte_pro.php
@@ -312,8 +312,8 @@ if (isset($_POST['modif_infos'])){
 
                     // Vérification si le bouton de génération est cliqué
                     if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['generate_api_key'])) {
-                        $prefix = 'M'; // Préfixe pour générer la clé
-                        $stmt = $dbh->prepare('SELECT tripenarvor.generate_api_key(:prefix)');
+                        $prefix = 'P'; // Préfixe pour générer la clé
+                        $stmt = $dbh->prepare('update tripenarvor._professionnel set api_key = tripenarvor.generate_api_key(:prefix) where code_compte = :code_compte');
                         $stmt->bindParam(':prefix', $prefix, PDO::PARAM_STR);
                         $stmt->execute();
 
