f506a4d232e749e4e49f829faa2eff8e921486d5
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-03-04 17:06:03 +0100

    Update creation_offre_spectacle_3.php

--- html/etape_3_boost/creation_offre_spectacle_3.php
+++ html/etape_3_boost/creation_offre_spectacle_3.php
@@ -42,7 +42,7 @@ if(isset($_POST['envoiForm4'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Offre - Spectacle</title>
-    <link rel="stylesheet" href="../creation_offre2.css">
+    <link rel="stylesheet" href="../styles.css">
 
 
 </head>
