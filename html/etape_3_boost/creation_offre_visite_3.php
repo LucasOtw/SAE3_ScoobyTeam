77761609d32f068b414be9642c765e6e1e6a650e
Maria DB <odin.rajic@etudiant.univ-rennes.fr>
2025-03-04 17:06:18 +0100

    Update creation_offre_visite_3.php

--- html/etape_3_boost/creation_offre_visite_3.php
+++ html/etape_3_boost/creation_offre_visite_3.php
@@ -40,7 +40,7 @@ if(isset($_POST['envoiForm3'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Offre - Visite</title>
-    <link rel="stylesheet" href="../creation_offre2.css">
+    <link rel="stylesheet" href="../styles.css">
 
 
 </head>
