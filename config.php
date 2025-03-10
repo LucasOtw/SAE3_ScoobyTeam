2c55288f1f8154e61af7a1f347304e7c6131504c
QuentinUg <152272314+Quentin-Uguen@users.noreply.github.com>
2025-03-10 14:29:00 +0100

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php'; // Charge les librairies installées par Composer
-
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 2)); // Remonte de 2 niveaux pour atteindre /docker/sae/
-$dotenv->load();
-?>
