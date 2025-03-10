f7d8bafcc972a06c4c2bc68735fe7e966c72529c
Gandalf1heGrey <etudiant@pce-test>
2025-03-10 14:07:37 +0100

    Creation Attractions - Validation (en cours)

--- html/etape_4_creation/creation_offre_attraction_4.php
+++ html/etape_4_creation/creation_offre_attraction_4.php
@@ -26,7 +26,9 @@ if(!isset($_SESSION['pro'])){
     exit;
 }
 
+echo "<pre>";
 var_dump($_SESSION);
+echo "</pre>";
 
 if (!isset($_POST['valider']) && !isset($_POST['valider_plus_tard'])) {
     // Vérifier si une ou plusieurs sessions nécessaires ne sont pas définies
