cca1af8ed4dac152d5c386894cb72e9ac59b98ab
Quentin Uguen <quguen@pce-test>
2025-03-10 11:37:20 +0100

    test .env7

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -1,21 +1,21 @@
-<?php 
+<?php
 
-        $dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
-        $username = "sae";
-        $password = "philly-Congo-bry4nt";
+$dsn = getenv("DB_DSN");
+$username = getenv("DB_USERNAME");
+$password = getenv("DB_PASSWORD");
 
-        // Créer une instance PDO
-        $dbh = new PDO($dsn, $username, $password);
+// Créer une instance PDO
+$dbh = new PDO($dsn, $username, $password);
 
-        // UPDATE DE BDD
+// UPDATE DE BDD
 
-        $var1 = "../images/offres/MeinKraft/minecraft.jpg";
-        $var2 = "images/offres/MeinKrafte/minecraft.jpg";
+$var1 = "../images/offres/MeinKraft/minecraft.jpg";
+$var2 = "images/offres/MeinKrafte/minecraft.jpg";
 
-        $req = $dbh->prepare("UPDATE tripenarvor._image SET url_image = :nouv_val WHERE url_image = :ancienne_val");
-        $req->bindValue(":nouv_val",$var2);
-        $req->bindValue(":ancienne_val",$var1);
-        if($req->execute()){
-                echo "Hello Mother !";
-        }
+$req = $dbh->prepare("UPDATE tripenarvor._image SET url_image = :nouv_val WHERE url_image = :ancienne_val");
+$req->bindValue(":nouv_val", $var2);
+$req->bindValue(":ancienne_val", $var1);
+if ($req->execute()) {
+        echo "Hello World !";
+}
 ?>
