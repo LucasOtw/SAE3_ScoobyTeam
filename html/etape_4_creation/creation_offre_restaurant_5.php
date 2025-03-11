c067b3d997cb642c00fba0de2f1ca8e2532292ed
Gandalf1heGrey <etudiant@pce-test>
2025-03-11 10:49:41 +0100

    Modifications creation_offre

--- html/etape_4_creation/creation_offre_restaurant_5.php
+++ html/etape_4_creation/creation_offre_restaurant_5.php
@@ -406,7 +406,7 @@ if(isset($_POST['valider']) || isset($_POST['passer_cb']) || isset($_POST['creer
     ?>
         <div class="fleche_retour">
             <div>
-                <a href="../etape_3_boost/creation_offre_attraction_3.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
+                <a href="../etape_3_boost/creation_offre_restaurant_4.php"><img src="../images/Bouton_retour.png" alt="retour"></a>
             </div>
         </div>
     
