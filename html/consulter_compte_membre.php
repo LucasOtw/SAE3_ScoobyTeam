cf3a8b9023332adecb47c3b3f36f9ea9f0fd1376
jmulliez <mulliezjoseph5@gmail.com>
2025-03-11 10:48:27 +0100

    style

--- html/consulter_compte_membre.php
+++ html/consulter_compte_membre.php
@@ -432,13 +432,7 @@ if (isset($_POST['dwl-data'])) {
                    <input type="text" id="ville" name="ville" value="<?php echo $_adresse['ville']; ?>" placeholder="Ville *" required>
                </fieldset>
             </div>
-            <div class="crea_pro_raison_sociale_num_siren2">
-            <fieldset style="display: flex;">
-                <legend>Clé API</legend>
-                <input disabled type="text" id="cle_api" name="cle_api" value="<?php echo htmlspecialchars($monCompteMembre['api_key']); ?>" readonly>
-            </fieldset>
-            <input type="submit" id="btn-api" name="generate_api_key" value="" alt="Regénérer la clé API">
-            </div>
+            
             <div class="checkbox">
                 <input type="checkbox" id="cgu" name="cgu" required>
                 <label for="cgu">J’accepte les <a href="cgu.html">Conditions générales d’utilisation (CGU)</a></label>
