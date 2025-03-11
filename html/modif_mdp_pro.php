a2e63e62b69d7cf2ae15e7b6c22fb0357f450077
jmulliez <mulliezjoseph5@gmail.com>
2025-03-11 11:44:57 +0100

    corrzection de la taille

--- html/modif_mdp_pro.php
+++ html/modif_mdp_pro.php
@@ -262,7 +262,7 @@ if (isset($_POST['modif_infos'])){
         </section>
         <fieldset id="api">
                 <legend>Clé API</legend>
-                <input disabled type="text" id="cle_api" name="cle_api" value="<?php echo htmlspecialchars($monComptePro['api_key']); ?>" readonly>
+                <input disabled type="text" id="cle_api_pro" name="cle_api" value="<?php echo htmlspecialchars($monComptePro['api_key']); ?>" readonly>
                 <input type="submit" id="btn-api-pro" name="generate_api_key" value="" alt="Regénérer la clé API">
         </fieldset>
         <form action="modif_mdp_pro.php" method="POST">
