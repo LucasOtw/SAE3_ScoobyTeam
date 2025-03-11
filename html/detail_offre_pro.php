1ed72371bde5242b5e7ed098a4e4fb30b960225e
Gandalf1heGrey <etudiant@pce-test>
2025-03-11 11:53:44 +0100

    Ajout du bouton de suppression d'une offre

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -391,6 +391,11 @@ include("recupInfosCompte.php");
                         <?php if (!empty($details_offre["site_web"])) { ?> <a
                                 href="<?php echo $details_offre["site_web"]; ?>" target="_blank"><button
                                     class="visit-button_detailoffre_pro">Voir le site ➔</button></a> <?php } ?>
+                        <form id="add-btn" action="#" method="POST">
+                            <input type="hidden" name="uneOffre"></input>
+                            <input type="submit" id="btn-voir-offre" class="button-text add-btn" name="supprOffre"
+                            value="Supprimer votre offre">
+                        </form>
                         <form id="add-btn" action="modifier_offre.php" method="POST">
                             <input type="hidden" name="uneOffre"
                                 value="<?php echo htmlspecialchars(serialize($details_offre)); ?>">
