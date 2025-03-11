304ea98bc876f90e1df7e3276ac59ac953ace18e
jmulliez <mulliezjoseph5@gmail.com>
2025-03-11 12:01:40 +0100

    style

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -141,7 +141,7 @@ if (isset($_POST['modif_infos'])){
                 <legend>Clé API</legend>
                 <input disabled type="text" id="cle_api" name="cle_api" value="<?php echo htmlspecialchars($monCompteMembre['api_key']); ?>" readonly>
                 <input type="submit" id="btn-api" name="generate_api_key" value="" alt="Regénérer la clé API">
-        </fieldset>
+        </fieldset>     
         <form action="modif_mdp_membre.php" method="POST">
            <h3>Modifiez votre mot de passe</h3>
            
