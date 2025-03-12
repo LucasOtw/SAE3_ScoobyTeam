5a01d93314ddd4526750d53a04544ab8d25141ca
Odin <etudiant@pce-test>
2025-03-12 18:02:26 +0100

    Suppression_offre

--- html/supprimer_offre.php
+++ html/supprimer_offre.php
@@ -4,8 +4,15 @@ ob_start();
 session_start();
 
 if(isset($_POST['uneOffre'])){
-    echo "t";
-    var_dump(unserialize($_POST['uneOffre']));
+    $uneOffre = unserialize($_POST['uneOffre']);
+
+    echo "<pre>";
+    var_dump($uneOffre);
+    echo "</pre>";
+} else {
+    // sinon il n'a rien à faire ici
+    header('location: mes_offres.php');
+    exit;
 }
 
 ?>
