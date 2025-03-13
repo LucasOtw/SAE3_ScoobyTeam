bb135646ff0745e02c2c781bcec96e798d730905
Odin <etudiant@pce-test>
2025-03-13 10:42:22 +0100

    pull

--- html/supprimer_offre.php
+++ html/supprimer_offre.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+*   RECETTE DE SUPPRESSION D'OFFRE
+*
+*   1) Récupérer le lien de chaque image dans la BDD
+*   2) Supprimer une offre de tripenarvor._offres
+*   3) Battre les oeufs
+*   4) Supprimer les images relatives à l'offre dans et ._image (DELETE CASCADE)
+*
+*/
+
 ob_start();
 session_start();
 
@@ -9,6 +19,8 @@ if(isset($_POST['uneOffre'])){
     echo "<pre>";
     var_dump($uneOffre);
     echo "</pre>";
+
+
 } else {
     // sinon il n'a rien à faire ici
     header('location: mes_offres.php');
