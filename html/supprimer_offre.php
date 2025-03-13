801a7f726e4644442f024538594a4987ed93a546
Odin <etudiant@pce-test>
2025-03-13 11:20:46 +0100

    Suppression_offre

--- html/supprimer_offre.php
+++ html/supprimer_offre.php
@@ -4,6 +4,7 @@
 *   RECETTE DE SUPPRESSION D'OFFRE
 *
 *   1) Récupérer le lien de chaque image dans la BDD
+*   2) Vérifier l'unicité de chaque code horaire
 *   2) Supprimer une offre de tripenarvor._offres
 *   3) Battre les oeufs
 *   4) Supprimer les images relatives à l'offre dans et ._image (DELETE CASCADE)
@@ -13,6 +14,13 @@
 ob_start();
 session_start();
 
+$dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
+$username = "sae";
+$password = "philly-Congo-bry4nt";
+
+// Créer une instance PDO
+$dbh = new PDO($dsn, $username, $password);
+
 if(isset($_POST['uneOffre'])){
     $uneOffre = unserialize($_POST['uneOffre']);
 
@@ -20,6 +28,19 @@ if(isset($_POST['uneOffre'])){
     var_dump($uneOffre);
     echo "</pre>";
 
+    // On va d'abord récupérer chaque lien d'image
+
+    $recupCodesImages = $dbh->prepare("SELECT lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche
+    FROM tripenarvor_offre WHERE code_offre = :code_offre");
+    $recupCodesImages->bindValue(':code_offre');
+    $recupCodesImages->execute();
+
+    $codesImages = $recupCodesImages->fetchColumn();
+    $codesImages = array_filter($codesImages, function($val){
+        return !is_null($val);
+    });
+
+    var_dump($codesImages);
 
 } else {
     // sinon il n'a rien à faire ici
