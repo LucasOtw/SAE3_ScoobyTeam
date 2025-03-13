355fb33625f868eb69087bb820f377e1edabc85e
Odin <etudiant@pce-test>
2025-03-13 11:26:55 +0100

    Suppression_offre

--- html/supprimer_offre.php
+++ html/supprimer_offre.php
@@ -30,17 +30,17 @@ if(isset($_POST['uneOffre'])){
 
     // On va d'abord récupérer chaque lien d'image
 
-    $recupCodesImages = $dbh->prepare("SELECT lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche
+    $recupCodesHoraires = $dbh->prepare("SELECT lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche
     FROM tripenarvor._offre WHERE code_offre = :code_offre");
-    $recupCodesImages->bindValue(':code_offre',$codeOffre);
-    $recupCodesImages->execute();
+    $recupCodesHoraires->bindValue(':code_offre',$codeOffre);
+    $recupCodesHoraires->execute();
 
-    $codesImages = $recupCodesImages->fetch(PDO::FETCH_ASSOC);
-    $codesImages = array_filter($codesImages, function($val){
+    $CodesHoraires = $recupCodesHoraires->fetch(PDO::FETCH_ASSOC);
+    $CodesHoraires = array_filter($CodesHoraires, function($val){
         return !is_null($val);
     });
 
-    var_dump($codesImages);
+    var_dump($codesHoraires);
 
 } else {
     // sinon il n'a rien à faire ici
