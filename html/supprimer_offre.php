dbce932f3e4ca2e76ec74d15e2327ecc69a197e6
Odin <etudiant@pce-test>
2025-03-13 11:46:01 +0100

    Suppression d'offre

--- html/supprimer_offre.php
+++ html/supprimer_offre.php
@@ -25,7 +25,17 @@ if(isset($_POST['uneOffre'])){
     $codeOffre = unserialize($_POST['uneOffre']);
 
 
-    // On va d'abord récupérer chaque lien d'image
+    /* RÉCUPÉRATION DE CHAQUE LIEN D'IMAGE */
+
+    $recupCodesImages = $dbh->prepare("SELECT code_image FROM tripenarvor._son_image
+    WHERE code_offre = :code_offre");
+    $recupCodesImages->bindValue(':code_offre',$codeOffre);
+    $recupCodesImages->execute();
+
+    $codesImages = $recupCodesImages->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($codesImages);
+
+    /* RÉCUPRATION ET VÉRIFICATION DE L'UNICITÉ DES CODES HORAIRES */
 
     $recupCodesHoraires = $dbh->prepare("SELECT lundi,mardi,mercredi,jeudi,vendredi,samedi,dimanche
     FROM tripenarvor._offre WHERE code_offre = :code_offre");
