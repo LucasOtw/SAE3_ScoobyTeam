bc99d3b1fd1e803cbc4fac39d407be2251271e98
anprieur <antoine.prieur@yahoo.fr>
2025-03-13 09:38:33 +0100

    Update detail_offre_pro.php

--- html/detail_offre_pro.php
+++ html/detail_offre_pro.php
@@ -914,7 +914,7 @@ include("recupInfosCompte.php");
                                     <div class="custom-modal-content">
                                         <p class="texte-boite-perso">Voulez-vous vraiment blacklister l'avis ?</p>
 
-                                        <p class="texte-boite-perso">Après il vous restera <?php var_dump($details_offre) ; ?> jeton(s)</p>
+                                        <p class="texte-boite-perso">Après il vous restera <?php echo $details_offre["nb_blacklister"]; ?> jeton(s)</p>
                                         
                                         <button id="cancelBlacklist" class="cancel-btn">Non</button>
                                         <button id="confirmBlacklist" class="confirm-btn">Oui</button>
