45ea1035986ff97861f87b87b64a744d85c4ef4b
vasamson <samson.valentin2005@gmail.com>
2025-03-13 09:51:50 +0100

    Update page404.php

--- html/page404.php
+++ html/page404.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page non trouvée</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" href="images/logoPin_orange.png" width="16px" height="32px">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=K2D:wght@100;200;300;400;500;600;700;800&display=swap" rel="stylesheet">
