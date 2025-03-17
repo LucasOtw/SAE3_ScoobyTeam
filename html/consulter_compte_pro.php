9b575d0b430f2566adaa88968d1b25c39d06cd73
vasamson <samson.valentin2005@gmail.com>
2025-03-17 11:11:25 +0100

    Update consulter_compte_pro.php

--- html/consulter_compte_pro.php
+++ html/consulter_compte_pro.php
@@ -157,9 +157,7 @@ if (isset($_POST['modif_infos'])){
                         ///                            Contenu notif                                ///
                         ///////////////////////////////////////////////////////////////////////////////
                         
-                        $dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
-                        $username = "sae";
-                        $password = "philly-Congo-bry4nt";
+                        require_once __DIR__ . ("/../.security/config.php");
                         
                         // Créer une instance PDO
                         $dbh = new PDO($dsn, $username, $password);
