66031815419e8f9a35a9832255743d82f41162f8
vasamson <samson.valentin2005@gmail.com>
2025-03-17 11:36:25 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -19,9 +19,7 @@ if (isset($_SESSION['detail_offre'])) {
 }
 
 
-$dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
-$username = "sae";
-$password = "philly-Congo-bry4nt";
+require_once __DIR__ . ("/../.security/config.php");
 
 // Créer une instance PDO
 $dbh = new PDO($dsn, $username, $password);
@@ -628,9 +626,7 @@ function tempsEcouleDepuisPublication($offre){
                     <section class="vu-recemment">
                     <?php
                         try {
-                            $dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
-                            $username = "sae";
-                            $password = "philly-Congo-bry4nt";
+                            require_once __DIR__ . ("/../.security/config.php");
             
                             // Créer une instance PDO
                             $dbh = new PDO($dsn, $username, $password);
