20a22c45a464ed56dce99235377b8af0d9cda614
vasamson <samson.valentin2005@gmail.com>
2025-03-17 11:36:58 +0100

    Update voir_toutes_les_offres.php

--- html/voir_toutes_les_offres.php
+++ html/voir_toutes_les_offres.php
@@ -19,9 +19,7 @@ if (isset($_SESSION['detail_offre'])) {
 }
 
 
-$dsn = "pgsql:host=postgresdb;port=5432;dbname=sae;";
-$username = "sae";
-$password = "philly-Congo-bry4nt";
+require_once __DIR__ . ("/../.security/config.php");
 
 // Créer une instance PDO
 $dbh = new PDO($dsn, $username, $password);
