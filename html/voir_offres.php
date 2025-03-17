33b51456f2237f090ffc5b1d03acfb866565e666
vasamson <samson.valentin2005@gmail.com>
2025-03-17 17:21:54 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1344,35 +1344,10 @@ function tempsEcouleDepuisPublication($offre){
             </div>
         </div>
     </footer>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <link rel="stylesheet" href="leaflet.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.1/dist/MarkerCluster.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.1/dist/MarkerCluster.Default.css" />
     <div id="map"></div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <script src="https://unpkg.com/leaflet.markercluster@1.5.1/dist/leaflet.markercluster.js"></script>
     <script>
