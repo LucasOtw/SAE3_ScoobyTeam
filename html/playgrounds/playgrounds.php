2830435a8e8b533777c79ddc9462d0ed5332e745
Odin <etudiant@pce-test>
2025-03-19 10:25:16 +0100

    Modif playgrounds

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -2,20 +2,6 @@
 
 require_once __DIR__ . ("/../../.security/config.php");
 
+var_dump($dbh);
 
-echo "DSN : " . $dsn;
-// Créer une instance PDO
-$dbh = new PDO($dsn, $username, $password);
-
-// UPDATE DE BDD
-
-$var1 = "../images/offres/MeinKraft/minecraft.jpg";
-$var2 = "images/offres/MeinKrafte/minecraft.jpg";
-
-$req = $dbh->prepare("UPDATE tripenarvor._image SET url_image = :nouv_val WHERE url_image = :ancienne_val");
-$req->bindValue(":nouv_val", $var2);
-$req->bindValue(":ancienne_val", $var1);
-if ($req->execute()) {
-        echo "Hello World !";
-}
 ?>
