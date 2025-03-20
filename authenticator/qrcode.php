26356703d91453aad61259a0c1df1623bc0ab698
vasamson <samson.valentin2005@gmail.com>
2025-03-20 11:07:22 +0100

    Update qrcode.php

--- authenticator/qrcode.php
+++ authenticator/qrcode.php
@@ -1,26 +1,20 @@
 <?php
-// Inclure l'autoloader de Composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Writer\PngWriter;
+use BaconQrCode\Renderer\Image\PngImageBackEnd;
+use BaconQrCode\Writer;
+use BaconQrCode\Renderer\Image\ImageRenderer;
+use BaconQrCode\Renderer\Image\RendererStyle\RendererStyle;
 
-// Le contenu que vous souhaitez coder dans le QR Code (par exemple, une URL)
-$data = "https://www.example.com";
+$writer = new Writer(
+    new ImageRenderer(
+        new RendererStyle(300), 
+        new PngImageBackEnd()
+    )
+);
 
-// Créer le QR Code
-$qrCode = new QrCode($data);
+$qrCode = $writer->writeString('https://www.example.com');
 
-// Définir des options pour la taille et la marge du QR Code
-$qrCode->setSize(300);
-$qrCode->setMargin(10);
-
-// Assurez-vous qu'il n'y a aucune sortie avant cet en-tête
-ob_clean();  // Efface les éventuels buffers de sortie
 header('Content-Type: image/png');
-
-// Utilisez le writer pour générer et afficher l'image du QR Code
-$writer = new PngWriter();
-echo $writer->write($qrCode)->getString();
-exit();  // Assurez-vous d'arrêter le script après avoir envoyé l'image
+echo $qrCode;
 ?>
