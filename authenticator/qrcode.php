8fc3a8fac160ee313f17e2168c89eea69789b9f9
Odin <etudiant@pce-test>
2025-03-20 10:55:48 +0100

    Test OTP

--- authenticator/qrcode.php
+++ authenticator/qrcode.php
@@ -1,6 +1,6 @@
 <?php
 // Inclure l'autoloader de Composer
-require '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Endroid\QrCode\QrCode;
 use Endroid\QrCode\Writer\PngWriter;
