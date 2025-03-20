6b44bd1a5f189c5471e86801539d46a6fa34b7fd
Odin <etudiant@pce-test>
2025-03-20 09:59:29 +0100

    Generation code secret

--- html/test_otp.php
+++ html/test_otp.php
@@ -3,8 +3,7 @@ echo "<h1>Test OTP</h1>";
 
 // Générer un secret OTP
 echo "<h2>1️⃣ Génération du secret OTP</h2>";
-$response = file_get_contents("../authenticator/generation_secret.php");
-echo "<pre>$response</pre>";
+include_once("../generation_secret.php");
 
 // Afficher le QR Code
 echo "<h2>2️⃣ QR Code (Scanne-le avec Google Authenticator)</h2>";
