b069517f69e3566b7433ef58022314a38af3d24a
Odin <etudiant@pce-test>
2025-03-21 11:24:08 +0100

    Modif modal

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -124,8 +124,8 @@ if(!empty($_POST)){
         padding: 20px;
         border: 1px solid #888;
         width: 40%;
-        left: 50%;
-        top: 50%;
+        right: 50%;
+        right: 50%;
         transform: translate(-50%,-50%);
         max-width: 400px;
         display: flex;
