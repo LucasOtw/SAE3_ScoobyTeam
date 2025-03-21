2c5f68e2ee730d872d35650e28577524b630bf1c
Odin <etudiant@pce-test>
2025-03-21 11:16:27 +0100

    Modif modal

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -107,10 +107,11 @@ if(!empty($_POST)){
     /* Style pour la Popup */
     .modal {
         display: none;
-        position: fixed;
+        position: absolute;
         z-index: 1;
-        left: 0;
-        top: 0;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%,-50%);
         width: 100%;
         height: 100%;
         overflow: auto;
