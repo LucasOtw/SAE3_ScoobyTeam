f33c1a59b13a3e2f5a72e988da473010b7698019
vasamson <samson.valentin2005@gmail.com>
2025-03-21 10:37:58 +0100

    Update connexion_membre.php

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -103,44 +103,7 @@ if(!empty($_POST)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Se connecter</title>
     <link rel="stylesheet" href="styles.css">
-    <style>
-        /* Style pour la Popup */
-        .modal {
-            display: none;
-            position: fixed;
-            z-index: 1;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            overflow: auto;
-            background-color: rgba(0, 0, 0, 0.4); /* Couleur de fond semi-transparente */
-        }
-
-        /* Contenu de la Popup */
-        .modal-content {
-            background-color: #fefefe;
-            margin: 15% auto;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 40%;
-        }
-
-        /* Le bouton de fermeture de la popup */
-        .close {
-            color: #aaa;
-            float: right;
-            font-size: 28px;
-            font-weight: bold;
-        }
-
-        .close:hover,
-        .close:focus {
-            color: black;
-            text-decoration: none;
-            cursor: pointer;
-        }
-    </style>
+    
 </head>
 
 <body>
