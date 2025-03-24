afcfad5464671911047417cbbd5aeaa54b62a1ed
Lucas Torres <lucas@dy133082.univ-rennes1.fr>
2025-03-24 14:45:54 +0100

    ajout

--- html/modif_mdp_pro.php
+++ html/modif_mdp_pro.php
@@ -358,7 +358,7 @@ if (isset($_POST['modif_infos'])) {
         ?>
 
         <div class="creation-success">
-            <img src="./images/check.svg" alt="Succès">
+            <img src="./images/icones/check.svg" alt="Succès">
             <h4>Compte créé avec succès ! Vous pouvez maintenant vous connecter.</h4>
             <button class="close-btn">&times;</button>
         </div>
