6f927b4594e67db1c6102e2ea539dff0f206e944
Lucas Torres <lucas@dy133082.univ-rennes1.fr>
2025-03-24 08:56:20 +0100

    pousuite 2fa

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -186,7 +186,7 @@ if(!empty($_POST)){
     <!--Pour l'affichage de la checkbox si 2FA activée-->
     <script>
         document.getElementById('enable2FA').addEventListener('change', function() {
-            var phrase = document.getElementById('phrase');
+            var phrase = document.querySelector('.info_2fa');
             if (this.checked) {
                 phrase.style.display = 'block';
             } else {
