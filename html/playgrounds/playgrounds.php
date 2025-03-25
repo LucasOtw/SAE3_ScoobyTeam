e080284515bad9cbcbf695782dbe0e7f4831dcb0
Odin <etudiant@pce-test>
2025-03-25 18:50:31 +0100

    Test getSecret playgrounds

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -1,20 +1,14 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../.security/config.php';
 
 use OTPHP\TOTP;
 
-$clock = "123456789";
+$getSecret = $dbh->prepare('SELECT code_secret FROM tripenarvor._compte_otp WHERE code_compte = 2');
+$getSecret->execute();
 
-// A random secret will be generated from this.
-// You should store the secret with the user for verification.
-$otp = TOTP::generate();
-echo "The OTP secret is: {$otp->getSecret()}\n";
-
-// Note: use your own way to load the user secret.
-// The function "load_user_secret" is simply a placeholder.
-$secret = load_user_secret();
-$otp = TOTP::createFromSecret($secret, $clock);
-echo "The current OTP is: {$otp->now()}\n";
+$secret = $getSecret->fetchColumn();
+var_dump($secret);
 
 ?>
