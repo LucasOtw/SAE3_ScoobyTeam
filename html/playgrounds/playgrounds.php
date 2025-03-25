35cb27c5d4c392437cafb1eef10ec6499ad27b4f
Odin <etudiant@pce-test>
2025-03-25 18:50:58 +0100

    Test getSecret playgrounds

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../.security/config.php';
+require_once __DIR__ . '/../../.security/config.php';
 
 use OTPHP\TOTP;
 
