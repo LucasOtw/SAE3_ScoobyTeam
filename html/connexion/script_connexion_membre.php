bb8f93013236ccccfc040e8b719fc6a266ce28e1
Odin <etudiant@pce-test>
2025-03-25 17:16:39 +0100

    Modif connexion_membre

--- html/connexion/script_connexion_membre.php
+++ html/connexion/script_connexion_membre.php
@@ -8,7 +8,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["mail"] ?? "";
     $password = $_POST["pwd"] ?? "";
 
-    if (empty($email) || empty($password)) {
+    echo json_encode(["success" => true, "message" => $email." / ".$password]);
+
+/*     if (empty($email) || empty($password)) {
         echo json_encode(["success" => false, "message" => "Tous les champs sont requis."]);
         exit;
     }
@@ -43,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Authentification réussie
     $_SESSION['membre'] = $user;
-    echo json_encode(["success" => true, "message" => "Identification autorisée"]);
+    echo json_encode(["success" => true, "message" => "Identification autorisée"]); */
 } else {
     echo json_encode(["success" => false, "message" => "Requête invalide."]);
 }
