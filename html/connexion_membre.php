7fb59e592e262b47d99b916e2f27e1a1582b5fdb
Odin <etudiant@pce-test>
2025-03-28 11:38:31 +0100

    Debug setTimeOut

--- html/connexion_membre.php
+++ html/connexion_membre.php
@@ -216,6 +216,8 @@ function hidePopup() {
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    localStorage.clear();
+
     var form = document.getElementById("connexionForm");
     var connectBtn = document.getElementById("connectButton");
     var submitBtn = document.getElementById("submitFormBtn");
@@ -375,6 +377,7 @@ function checkLockTime() {
     }
 
     // Utilisation de setTimeout pour exécuter cette fonction régulièrement
+    console.log(localStorage.getItem("essais_user"));
     setTimeout(checkLockTime, 1000); // Rappel la fonction toutes les secondes
 }
 
