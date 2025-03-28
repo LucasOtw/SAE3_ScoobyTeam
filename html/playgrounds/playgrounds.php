bcdb45a427c9a5ad99975763e40abe48d09aa9f7
Odin <etudiant@pce-test>
2025-03-28 15:20:23 +0100

    Playgrounds ZIP

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -1,20 +1,7 @@
 <?php
 
 require __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../../.security/config.php';
 
-use OTPHP\TOTP;
-
-$getSecret = $dbh->prepare('SELECT code_secret FROM tripenarvor._compte_otp WHERE code_compte = 2');
-$getSecret->execute();
-
-$secret = $getSecret->fetchColumn();
-var_dump($secret);
-
-$input = "864381";
-
-$otp = TOTP::create();
-$otp->setSecret($secret);
-var_dump($otp->verify($input,null,1));
+$zip = new ZipArchive();
 
 ?>
