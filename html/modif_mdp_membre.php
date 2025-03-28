0836eaf4513b1a896d6511a1639649a13aa4cd82
jmulliez <mulliezjoseph5@gmail.com>
2025-03-28 09:09:18 +0100

    test

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -93,6 +93,20 @@ if (isset($_POST['modif_infos'])) {
 <body>
     <header class="header_membre">
         <div class="logo">
+        <svg
+            width="428"
+            height="202"
+            viewBox="0 0 428 202"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            >
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0 126.87L0 0H428V134.241C374.61 176.076 300.465 202 218.5 202C131.823 202 53.891 173.01 0 126.87Z"
+                fill="#E8E8E8"
+            ></path>
+            </svg>
          <a href="voir_offres.php">
             <img src="images/logoBlanc.png" alt="PACT Logo">
          </a>
@@ -398,29 +412,5 @@ if (isset($_POST['modif_infos'])) {
         });
     });
 </script>
-<script>
-document.addEventListener("DOMContentLoaded", function () {
-    const mdpActuel = document.getElementById("mdp_actuel");
-    const mdpNv1 = document.getElementById("mdp_nv1");
-    const mdpNv2 = document.getElementById("mdp_nv2");
-    const submitBtn = document.querySelector(".submit-btn2");
-
-    function toggleButtonState() {
-        if (mdpActuel.value.trim() !== "" && mdpNv1.value.trim() !== "" && mdpNv2.value.trim() !== "") {
-            submitBtn.removeAttribute("disabled");
-        } else {
-            submitBtn.setAttribute("disabled", "disabled");
-        }
-    }
-
-    mdpActuel.addEventListener("input", toggleButtonState);
-    mdpNv1.addEventListener("input", toggleButtonState);
-    mdpNv2.addEventListener("input", toggleButtonState);
-
-    // Désactiver le bouton au chargement de la page
-    toggleButtonState();
-});
-</script>
-
 </body>
 </html>
