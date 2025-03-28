f9c63eafc8adfd39bd1503cb502448af58bdd644
jmulliez <mulliezjoseph5@gmail.com>
2025-03-28 09:11:11 +0100

    test

--- html/modif_mdp_membre.php
+++ html/modif_mdp_membre.php
@@ -93,20 +93,7 @@ if (isset($_POST['modif_infos'])) {
 <body>
     <header class="header_membre">
         <div class="logo">
-        <svg
-            width="428"
-            height="202"
-            viewBox="0 0 428 202"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            >
-            <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M0 126.87L0 0H428V134.241C374.61 176.076 300.465 202 218.5 202C131.823 202 53.891 173.01 0 126.87Z"
-                fill="#E8E8E8"
-            ></path>
-            </svg>
+        
          <a href="voir_offres.php">
             <img src="images/logoBlanc.png" alt="PACT Logo">
          </a>
