a5a76d50b4ae48b47b6e81ddb7ebe7520da4a0e2
Lucas Torres <lucas@MacBook-Pro-de-Lucas.local>
2025-03-29 18:23:28 +0100

    ajut

--- html/connexion_pro.php
+++ html/connexion_pro.php
@@ -114,7 +114,7 @@ session_start();
     <div id="modal-otp" class="otp-confirm-content2">
         <form id="envoiCode" action="#" method="POST">
             <p class="texte-boite-perso">Veuillez renseigner le code à 6 chiffres présent dans l'application :</p>
-            <input type="text" name="code_otp" id="otpCode" placeholder="Code à 6 chiffres" maxlength="6">
+            <input style="margin-top: 41px; margin-left: 170px; background-color: var(--orange);" type="text" name="code_otp" id="otpCode" placeholder="Code à 6 chiffres" maxlength="6">
             <input type="submit" id="submit-code" value="Envoyer le code">
             <p id="errorMsg" style="color: red; display: none;">Le code doit contenir exactement 6 chiffres.</p>
             <!--<button>Se connecter quand même</button>-->
