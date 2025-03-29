d4379ef2b2fb04191ede3b41f7e0883d861dd5b9
anprieur <antoine.prieur@yahoo.fr>
2025-03-29 16:39:42 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1203,19 +1203,19 @@ function tempsEcouleDepuisPublication($offre)
                             echo "        this._icon.setAttribute('data-offer', '" . htmlspecialchars(json_encode($monOffre), ENT_QUOTES, 'UTF-8') . "');";
                             echo "        console.log('Icône affichée avec data-offer :', this._icon);";
                             
-                            echo "        let offerData = this._icon.getAttribute('data-offer');";
+                            echo "        let offerData = ".$monOffre.";";
                             echo "        if (offerData) {";
-                            echo "            // Décodage des entités HTML dans le JSON";
-                            echo "            let parser = new DOMParser();";
-                            echo "            let doc = parser.parseFromString(offerData, 'text/html');";
-                            echo "            let decodedOfferData = doc.body.textContent || doc.body.innerText;"; // Décodage des entités HTML";
-                            echo "            let offer = JSON.parse(decodedOfferData);";
+                            // echo "            // Décodage des entités HTML dans le JSON";
+                            // echo "            let parser = new DOMParser();";
+                            // echo "            let doc = parser.parseFromString(offerData, 'text/html');";
+                            // echo "            let decodedOfferData = doc.body.textContent || doc.body.innerText;"; // Décodage des entités HTML";
+                            // echo "            let offer = JSON.parse(decodedOfferData);";
                             
                             echo "            ///////////////////////////////////////////////////";
                             echo "            ///            Barre de recherche               ///";
                             echo "            ///////////////////////////////////////////////////";    
-                            echo "            query = document.querySelector('.search-input').value.toLowerCase().trim();";
-                            echo "            let offerText = offer.titre_offre.toLowerCase();";
+                            echo "            let query = document.querySelector('.search-input').value.toLowerCase().trim();";
+                            echo "            let offerText = " . $monOffre["titre_offre"].toLowerCase() . ";";
                             echo "            if (!offerText.includes(query)) {";
                             //echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
                             echo "            } else {";
