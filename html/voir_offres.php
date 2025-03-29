1351f590c10810b60a8b47ae5dbdaa665647beef
anprieur <antoine.prieur@yahoo.fr>
2025-03-29 19:33:24 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1375,10 +1375,11 @@ function tempsEcouleDepuisPublication($offre)
                             ///                  Affichage                  ///
                             ///////////////////////////////////////////////////
                             echo "            if (afficher > 0) {";
-                            // echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
+                            echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
                             echo "                markers.removeLayer(marker);";
                             echo "             } else {";
-                            // echo "                this._icon.style.display = 'block';"; // Afficher le marqueur si le texte correspond
+                            echo "                this._icon.style.display = 'block';"; // Afficher le marqueur si le texte correspond
+                            echo "                markers.addLayer(marker);";
                             echo "             }";
 
                             echo "    }";
