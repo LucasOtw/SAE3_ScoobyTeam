b1efa31cefecdf832affdf3745b9a10142cb2fc3
anprieur <antoine.prieur@yahoo.fr>
2025-03-29 16:35:16 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1200,27 +1200,31 @@ function tempsEcouleDepuisPublication($offre)
                             echo "var marker = L.marker([$latitude, $longitude], {icon: customIcon});";
                             echo "marker.on('add', function() {";
                             echo "    if (this._icon) {";
-                            echo "        this._icon.setAttribute('data-offer', '" . htmlspecialchars(json_encode($monOffre)) . "');";
+                            echo "        this._icon.setAttribute('data-offer', '" . htmlspecialchars(json_encode($monOffre), ENT_QUOTES, 'UTF-8') . "');";
                             echo "        console.log('Icône affichée avec data-offer :', this._icon);";
-                            echo "        let offerData = this._icon.getAttribute('data-offer');";
-                            echo "             if (offerData) {";
-                            echo "                 let correctedJsonString = offerData.replace(/&quot;/g, '"').replace(/&#039;/g, "'"); ";
-                            echo "                 let offer = JSON.parse(correctedJsonString);";
-
-                            ///////////////////////////////////////////////////
-                            ///            Barre de recherche               ///
-                            ///////////////////////////////////////////////////    
-                            echo "                 query = document.querySelector('.search-input').value.toLowerCase().trim();";
-                            echo "                 let offerText = offer.titre_offre.toLowerCase();";
-                            echo "                 if (!offerText.includes(query)) {";
-                            echo "                     this._icon.style.display = 'none';";
-                            echo "                 } else {";
-                            echo "                     this._icon.style.display = 'block';";
-                            echo "                 }";
                             
-                            echo "             }";
+                            echo "        let offerData = this._icon.getAttribute('data-offer');";
+                            echo "        if (offerData) {";
+                            echo "            // Décodage des entités HTML dans le JSON";
+                            echo "            let parser = new DOMParser();";
+                            echo "            let doc = parser.parseFromString(offerData, 'text/html');";
+                            echo "            let decodedOfferData = doc.body.textContent || doc.body.innerText;"; // Décodage des entités HTML";
+                            echo "            let offer = JSON.parse(decodedOfferData);";
+                            
+                            echo "            ///////////////////////////////////////////////////";
+                            echo "            ///            Barre de recherche               ///";
+                            echo "            ///////////////////////////////////////////////////";    
+                            echo "            query = document.querySelector('.search-input').value.toLowerCase().trim();";
+                            echo "            let offerText = offer.titre_offre.toLowerCase();";
+                            echo "            if (!offerText.includes(query)) {";
+                            echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
+                            echo "            } else {";
+                            echo "                this._icon.style.display = 'block';"; // Afficher le marqueur si le texte correspond
+                            echo "            }";
+                            echo "        }";
                             echo "    }";
                             echo "});";
+
                             
                             echo "var popup = L.popup({closeButton: false, autoClose: false, closeOnClick: false, className: 'custom-popup'}).setContent(\"" . addslashes($popupContent) . "\");";
                             echo "marker.bindPopup(popup);";
