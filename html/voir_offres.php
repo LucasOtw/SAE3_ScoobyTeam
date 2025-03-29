b19e8cd2614adb59bcf1791143396e7dfce8413d
anprieur <antoine.prieur@yahoo.fr>
2025-03-29 17:52:48 +0100

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1234,11 +1234,11 @@ function tempsEcouleDepuisPublication($offre)
                             ///////////////////////////////////////////////////
                             ///                  Affichage                  ///
                             ///////////////////////////////////////////////////
-                            echo "            if (afficher > 0) {";
-                            echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
-                            echo "             } else {";
-                            echo "                this._icon.style.display = 'block';"; // Afficher le marqueur si le texte correspond
-                            echo "             }";
+                            // echo "            if (afficher > 0) {";
+                            // echo "                this._icon.style.display = 'none';";  // Cacher le marqueur si le texte ne correspond pas
+                            // echo "             } else {";
+                            // echo "                this._icon.style.display = 'block';"; // Afficher le marqueur si le texte correspond
+                            // echo "             }";
 
 
                             echo "    }";
