26d90f86c635b2f7d9006b8b907ac5224b16cf4a
Quentin Uguen <quguen@pce-test>
2025-04-01 22:50:17 +0200

    modif couleur loupe

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -236,7 +236,7 @@ function tempsEcouleDepuisPublication($offre)
                 <input type="text" class="search-input" placeholder="Rechercher parmi les offres">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                     stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                    style="color: white;">
+                    style="color: #898b8d;">
                     <circle cx="11" cy="11" r="8"></circle>
                     <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
                 </svg>
