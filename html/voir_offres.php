24153efa9680f641062ff3f8602b4ea692329468
anprieur <antoine.prieur@yahoo.fr>
2025-04-01 09:00:57 +0200

    Update voir_offres.php

--- html/voir_offres.php
+++ html/voir_offres.php
@@ -1291,6 +1291,7 @@ function tempsEcouleDepuisPublication($offre)
                                 $eventOffre->execute();
 
                                 $event = ($eventOffre->fetch(PDO::FETCH_ASSOC));
+                                $event = $event["date_".$type_offre];
                             } else {
                                 $event = "";
                             }
@@ -1336,7 +1337,7 @@ function tempsEcouleDepuisPublication($offre)
                             $popupContent .= "</div>";
                             $popupContent .= "</div>";
 
-                            echo "var marker = L.marker([$latitude, $longitude], {icon: customIcon, dataOffer: '" . htmlspecialchars(json_encode($monOffre), ENT_QUOTES, 'UTF-8') . "',dataStatus: '" . json_encode($dataStatusEng) . "', dataCategory: '" . json_encode($type_offre) . "', dataCity: '" . json_encode($villeOffre) . "', dataEvent: '" . json_encode(if (!empty($event)) { echo $event['date_' . $type_offre]; } else { echo ""; }) . "'});";
+                            echo "var marker = L.marker([$latitude, $longitude], {icon: customIcon, dataOffer: '" . htmlspecialchars(json_encode($monOffre), ENT_QUOTES, 'UTF-8') . "',dataStatus: '" . json_encode($dataStatusEng) . "', dataCategory: '" . json_encode($type_offre) . "', dataCity: '" . json_encode($villeOffre) . "', dataEvent: '" . json_encode($event) . "'});";
                             echo "markersArray.push(marker);";
 
                             echo "var popup = L.popup({closeButton: false, autoClose: false, closeOnClick: false, className: 'custom-popup'}).setContent(\"" . addslashes($popupContent) . "\");";
