b71b7d623cb1d024fa2e48cadb79f761efadf352
Odin <etudiant@pce-test>
2025-04-05 10:48:30 +0200

    Test isActiveOTP

--- html/playgrounds/playgrounds.php
+++ html/playgrounds/playgrounds.php
@@ -2,6 +2,12 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-$zip = new ZipArchive();
+$isActiveOTP = $dbh->prepare('SELECT is_active FROM tripenarvor._compte_otp WHERE code_compte = :code_compte');
+$isActiveOTP->bindValue(":code_compte", 2);
+$isActiveOTP->execute();
+
+$isActiveOTP = $isActiveOTP->fetchColumn();
+
+var_dump($isActiveOTP);
 
 ?>
